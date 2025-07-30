82e45227de96430ab353310081e523970f46b7ca
snpmyn <snpmyn@126.com>
2025-07-30 20:49:56 +0800

    提交

---
+++ core/src/main/java/fragmentation/fragmentation/helper/internal/TransactionRecord.java
@@ -0,0 +1,30 @@
+package fragmentation.fragmentation.helper.internal;
+
+import android.view.View;
+
+import java.util.ArrayList;
+
+/**
+ * @decs: TransactionRecord
+ * @author: 郑少鹏
+ * @date: 2019/5/20 9:34
+ */
+public final class TransactionRecord {
+    public String tag;
+    public int targetFragmentEnter = Integer.MIN_VALUE;
+    public int currentFragmentPopExit = Integer.MIN_VALUE;
+    public int currentFragmentPopEnter = Integer.MIN_VALUE;
+    public int targetFragmentExit = Integer.MIN_VALUE;
+    public boolean doNotAddToBackStack = false;
+    public ArrayList<SharedElement> sharedElementList;
+
+    public static class SharedElement {
+        public View sharedElement;
+        public String sharedName;
+
+        public SharedElement(View sharedElement, String sharedName) {
+            this.sharedElement = sharedElement;
+            this.sharedName = sharedName;
+        }
+    }
+}
