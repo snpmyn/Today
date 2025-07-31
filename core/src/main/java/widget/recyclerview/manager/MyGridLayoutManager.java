de7c6eda5c782f8c41efb59201b48d79052f0923
snpmyn <snpmyn@126.com>
2025-07-31 22:08:30 +0800

    提交

---
+++ core/src/main/java/widget/recyclerview/manager/MyGridLayoutManager.java
@@ -0,0 +1,70 @@
+package widget.recyclerview.manager;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Created on 2017/11/19 0019.
+ *
+ * @author 郑少鹏
+ * @desc MyGridLayoutManager
+ */
+public class MyGridLayoutManager extends GridLayoutManager {
+    /**
+     * 布局子控件监听
+     */
+    private final OnLayoutChildrenListener onLayoutChildrenListener;
+    /**
+     * 允滑
+     */
+    private boolean scrollEnable = true;
+
+    /**
+     * Creates a vertical GridLayoutManager
+     *
+     * @param context                  Current context, will be used to access resources.
+     * @param spanCount                The number of columns in the grid
+     * @param onLayoutChildrenListener The listener of layout children
+     */
+    public MyGridLayoutManager(Context context, int spanCount, OnLayoutChildrenListener onLayoutChildrenListener) {
+        super(context, spanCount);
+        this.onLayoutChildrenListener = onLayoutChildrenListener;
+    }
+
+    /**
+     * 允滑
+     *
+     * @param flag 允滑
+     */
+    public void setScrollEnable(boolean flag) {
+        this.scrollEnable = flag;
+    }
+
+    @Override
+    public boolean canScrollVertically() {
+        return scrollEnable && super.canScrollVertically();
+    }
+
+    @Override
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+        super.onLayoutChildren(recycler, state);
+        if (null != onLayoutChildrenListener) {
+            onLayoutChildrenListener.onLayoutChildren(recycler, state);
+        }
+    }
+
+    /**
+     * 布局子控件监听
+     */
+    public interface OnLayoutChildrenListener {
+        /**
+         * 布局子控件
+         *
+         * @param recycler RecyclerView.Recycler
+         * @param state    RecyclerView.State
+         */
+        void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state);
+    }
+}
---
+++ core/src/main/java/widget/recyclerview/manager/MyLinearLayoutManager.java
@@ -0,0 +1,68 @@
+package widget.recyclerview.manager;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Created on 2019/6/26.
+ *
+ * @author 郑少鹏
+ * @desc MyLinearLayoutManager
+ */
+public class MyLinearLayoutManager extends LinearLayoutManager {
+    /**
+     * 布局子控件监听
+     */
+    private final OnLayoutChildrenListener onLayoutChildrenListener;
+
+    /**
+     * Creates a vertical LinearLayoutManager
+     *
+     * @param context                  Current context, will be used to access resources.
+     * @param onLayoutChildrenListener The listener of layout children
+     */
+    public MyLinearLayoutManager(Context context, OnLayoutChildrenListener onLayoutChildrenListener) {
+        super(context);
+        this.onLayoutChildrenListener = onLayoutChildrenListener;
+    }
+
+    /**
+     * @param context                  Current context, will be used to access resources.
+     * @param orientation              Layout orientation. Should be {@link #HORIZONTAL} or {@link #VERTICAL}.
+     * @param reverseLayout            When set to true, layouts from end to start.
+     * @param onLayoutChildrenListener The listener of layout children
+     */
+    public MyLinearLayoutManager(Context context, int orientation, boolean reverseLayout, OnLayoutChildrenListener onLayoutChildrenListener) {
+        super(context, orientation, reverseLayout);
+        this.onLayoutChildrenListener = onLayoutChildrenListener;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param recycler
+     * @param state
+     */
+    @Override
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+        super.onLayoutChildren(recycler, state);
+        if (null != onLayoutChildrenListener) {
+            onLayoutChildrenListener.onLayoutChildren(recycler, state);
+        }
+    }
+
+    /**
+     * 布局子控件监听
+     */
+    public interface OnLayoutChildrenListener {
+        /**
+         * 布局子控件
+         *
+         * @param recycler RecyclerView.Recycler
+         * @param state    RecyclerView.State
+         */
+        void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state);
+    }
+}
