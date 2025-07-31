dafb8be971703fc1f9cf0619cd61d9b597f66333
snpmyn <snpmyn@126.com>
2025-07-31 13:07:23 +0800

    提交

---
+++ core/src/main/java/litepal/value/LitePalMagic.java
@@ -0,0 +1,14 @@
+package litepal.value;
+
+/**
+ * Created on 2018/3/5.
+ *
+ * @author 郑少鹏
+ * @desc 组件魔法值
+ */
+public class LitePalMagic {
+    /**
+     * String
+     */
+    public static final String STRING_WHIPPLETREE = "-";
+}
