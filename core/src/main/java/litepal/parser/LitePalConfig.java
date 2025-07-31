253bf4a0a17b53b7b40150a31792d2ec3cb9e261
snpmyn <snpmyn@126.com>
2025-07-31 08:46:53 +0800

    提交

---
+++ core/src/main/java/litepal/parser/LitePalConfig.java
@@ -0,0 +1,94 @@
+package litepal.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Model for litepal.xml configuration file.
+ *
+ * @author guolin
+ * @since 2016/11/10
+ */
+public class LitePalConfig {
+    /**
+     * The version of database.
+     */
+    private int version;
+    /**
+     * The name of database.
+     */
+    private String dbName;
+    /**
+     * The case of table names and column names and SQL.
+     */
+    private String cases;
+    /**
+     * Define where the .db file should be. Option values: internal external.
+     */
+    private String storage;
+    /**
+     * All the model classes that want to map in the database. Each class should
+     * be given the full name including package name.
+     */
+    private List<String> classNames;
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public String getStorage() {
+        return storage;
+    }
+
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
+
+    /**
+     * Get the class name list. Always add table_schema as a value.
+     *
+     * @return The class name list.
+     */
+    public List<String> getClassNames() {
+        if (classNames == null) {
+            classNames = new ArrayList<>();
+            classNames.add("litepal.model.Table_Schema");
+        } else if (classNames.isEmpty()) {
+            classNames.add("litepal.model.Table_Schema");
+        }
+        return classNames;
+    }
+
+    /**
+     * Add a class name into the current mapping model list.
+     *
+     * @param className Full package class name.
+     */
+    public void addClassName(String className) {
+        getClassNames().add(className);
+    }
+
+    public void setClassNames(List<String> classNames) {
+        this.classNames = classNames;
+    }
+
+    public String getCases() {
+        return cases;
+    }
+
+    public void setCases(String cases) {
+        this.cases = cases;
+    }
+}
---
+++ core/src/main/java/litepal/parser/LitePalContentHandler.java
@@ -0,0 +1,92 @@
+package litepal.parser;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * This is the content handler for analysis the litepal.xml file by SAXParser,
+ * and temporarily the only correct way to generate LitePalAttr model with
+ * values.
+ *
+ * @author Tony Green
+ * @since 1.0
+ */
+public class LitePalContentHandler extends DefaultHandler {
+    /**
+     * Store the parsed value of litepal.xml.
+     */
+    private LitePalAttr litePalAttr;
+
+    /**
+     * Characters in the characters tag. Decide to not use this method
+     * temporarily. Use value attribute instead.
+     */
+    @Override
+    public void characters(char[] ch, int start, int length) {
+
+    }
+
+    /**
+     * End of the document. Doing nothing temporarily.
+     */
+    @Override
+    public void endDocument() {
+
+    }
+
+    /**
+     * End of the element. Doing nothing temporarily.
+     */
+    @Override
+    public void endElement(String uri, String localName, String qName) {
+
+    }
+
+    /**
+     * Start of the document. Generate a LitePalAttr model at the same time.
+     */
+    @Override
+    public void startDocument() {
+        litePalAttr = LitePalAttr.getInstance();
+        litePalAttr.getClassNames().clear();
+    }
+
+    /**
+     * Start analysis the litepal.xml file. Set all the parsed value into the
+     * LitePalAttr model.
+     */
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
+        if (LitePalParser.NODE_DB_NAME.equalsIgnoreCase(localName)) {
+            for (int i = 0; i < attributes.getLength(); i++) {
+                if (LitePalParser.ATTR_VALUE.equalsIgnoreCase(attributes.getLocalName(i))) {
+                    litePalAttr.setDbName(attributes.getValue(i).trim());
+                }
+            }
+        } else if (LitePalParser.NODE_VERSION.equalsIgnoreCase(localName)) {
+            for (int i = 0; i < attributes.getLength(); i++) {
+                if (LitePalParser.ATTR_VALUE.equalsIgnoreCase(attributes.getLocalName(i))) {
+                    litePalAttr.setVersion(Integer.parseInt(attributes.getValue(i).trim()));
+                }
+            }
+        } else if (LitePalParser.NODE_MAPPING.equalsIgnoreCase(localName)) {
+            for (int i = 0; i < attributes.getLength(); i++) {
+                if (LitePalParser.ATTR_CLASS.equalsIgnoreCase(attributes.getLocalName(i))) {
+                    litePalAttr.addClassName(attributes.getValue(i).trim());
+                }
+            }
+        } else if (LitePalParser.NODE_CASES.equalsIgnoreCase(localName)) {
+            for (int i = 0; i < attributes.getLength(); i++) {
+                if (LitePalParser.ATTR_VALUE.equalsIgnoreCase(attributes.getLocalName(i))) {
+                    litePalAttr.setCases(attributes.getValue(i).trim());
+                }
+            }
+        } else if (LitePalParser.NODE_STORAGE.equalsIgnoreCase(localName)) {
+            for (int i = 0; i < attributes.getLength(); i++) {
+                if (LitePalParser.ATTR_VALUE.equalsIgnoreCase(attributes.getLocalName(i))) {
+                    litePalAttr.setStorage(attributes.getValue(i).trim());
+                }
+            }
+        }
+    }
+}
