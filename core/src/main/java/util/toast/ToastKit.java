077aa7f9d0f5f87b99051179b5114cf400d859d4
snpmyn <snpmyn@126.com>
2025-07-31 21:54:31 +0800

    提交

---
+++ core/src/main/java/util/toast/ToastKit.java
@@ -0,0 +1,48 @@
+package util.toast;
+
+import android.widget.Toast;
+
+import pool.application.BasePoolApp;
+
+/**
+ * Created on 2020-09-09
+ *
+ * @author zsp
+ * @desc ToastKit
+ */
+public class ToastKit {
+    private static Toast toast;
+
+    public static void showShort(CharSequence msg) {
+        show(msg, Toast.LENGTH_SHORT);
+    }
+
+    public static void showShortWithGravity(CharSequence msg, int gravity) {
+        show(msg, Toast.LENGTH_SHORT, gravity);
+    }
+
+    public static void showLong(CharSequence msg) {
+        show(msg, Toast.LENGTH_LONG);
+    }
+
+    public static void showLongWithGravity(CharSequence msg, int gravity) {
+        show(msg, Toast.LENGTH_LONG, gravity);
+    }
+
+    private static void show(CharSequence msg, int time) {
+        if (null != toast) {
+            toast.cancel();
+        }
+        toast = Toast.makeText(BasePoolApp.getBasePoolAppInstance(), msg, time);
+        toast.show();
+    }
+
+    private static void show(CharSequence msg, int time, int gravity) {
+        if (null != toast) {
+            toast.cancel();
+        }
+        toast = Toast.makeText(BasePoolApp.getBasePoolAppInstance(), msg, time);
+        toast.setGravity(gravity, 0, 0);
+        toast.show();
+    }
+}
---
+++ core/src/main/java/util/typeface/TypefaceUtils.java
@@ -0,0 +1,114 @@
+package util.typeface;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created on 2021/4/15
+ *
+ * @author zsp
+ * @desc Typeface 工具类
+ */
+public class TypefaceUtils {
+    /**
+     * SansBold
+     *
+     * @param context 上下文
+     * @return Typeface
+     */
+    public static Typeface sansBold(@NotNull Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "OpenSans-Bold.ttf");
+    }
+
+    /**
+     * SansBoldItalic
+     *
+     * @param context 上下文
+     * @return Typeface
+     */
+    public static Typeface sansBoldItalic(@NotNull Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "OpenSans-BoldItalic.ttf");
+    }
+
+    /**
+     * SansExtraBold
+     *
+     * @param context 上下文
+     * @return Typeface
+     */
+    public static Typeface sansExtraBold(@NotNull Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "OpenSans-ExtraBold.ttf");
+    }
+
+    /**
+     * SansExtraBoldItalic
+     *
+     * @param context 上下文
+     * @return Typeface
+     */
+    public static Typeface sansExtraBoldItalic(@NotNull Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "OpenSans-ExtraBoldItalic.ttf");
+    }
+
+    /**
+     * SansItalic
+     *
+     * @param context 上下文
+     * @return Typeface
+     */
+    public static Typeface sansItalic(@NotNull Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "OpenSans-Italic.ttf");
+    }
+
+    /**
+     * SansLight
+     *
+     * @param context 上下文
+     * @return Typeface
+     */
+    public static Typeface sansLight(@NotNull Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "OpenSans-Light.ttf");
+    }
+
+    /**
+     * SansLightItalic
+     *
+     * @param context 上下文
+     * @return Typeface
+     */
+    public static Typeface sansLightItalic(@NotNull Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "OpenSans-LightItalic.ttf");
+    }
+
+    /**
+     * SansRegular
+     *
+     * @param context 上下文
+     * @return Typeface
+     */
+    public static Typeface sansRegular(@NotNull Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "OpenSans-Regular.ttf");
+    }
+
+    /**
+     * SansSemibold
+     *
+     * @param context 上下文
+     * @return Typeface
+     */
+    public static Typeface sansSemibold(@NotNull Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "OpenSans-Semibold.ttf");
+    }
+
+    /**
+     * SansSemiboldItalic
+     *
+     * @param context 上下文
+     * @return Typeface
+     */
+    public static Typeface sansSemiboldItalic(@NotNull Context context) {
+        return Typeface.createFromAsset(context.getAssets(), "OpenSans-SemiboldItalic.ttf");
+    }
+}
