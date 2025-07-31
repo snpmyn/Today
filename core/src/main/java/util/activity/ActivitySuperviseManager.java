e17fe521b3f3ca302a4b3403f156de6ffacf4dd4
snpmyn <snpmyn@126.com>
2025-07-31 21:34:29 +0800

    提交

---
+++ core/src/main/java/util/activity/ActivitySuperviseManager.java
@@ -0,0 +1,180 @@
+package util.activity;
+
+import static android.content.Context.ACTIVITY_SERVICE;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.Application;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import util.datetime.CurrentTimeMillisClock;
+import util.toast.ToastKit;
+import timber.log.Timber;
+
+/**
+ * Created on 2017/9/19.
+ *
+ * @author 郑少鹏
+ * @desc ActivitySuperviseManager
+ * 使用一：
+ * {@link Application#registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks)} 之 onActivityCreated 当 {@link AppCompatActivity#onCreate(Bundle, PersistableBundle)} 时执行，android:launchMode="singleTask" 时不执行。
+ * {@link Application#registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks)} 之 onActivityDestroyed 当 {@link AppCompatActivity#finish()(Bundle, PersistableBundle)} 时执行，android:launchMode="singleTask" 时不执行。
+ * <p>
+ * 使用二：
+ * 基类之 {@link AppCompatActivity#onCreate(Bundle, PersistableBundle)} 推当前 Activity 至 Activity 管理容器，需时遍历容器并 finish 所有 Activity。
+ */
+public class ActivitySuperviseManager {
+    private static final long WAIT_TIME = 2000L;
+    private final List<Activity> ACTIVITIES = Collections.synchronizedList(new LinkedList<>());
+    private long touchDownTime = 0L;
+
+    public static ActivitySuperviseManager getInstance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    /**
+     * 推 Activity 至堆栈
+     *
+     * @param activity Activity
+     */
+    public void pushActivity(Activity activity) {
+        ACTIVITIES.add(activity);
+        Timber.d("推入：%s", activity.getClass().getSimpleName());
+        Timber.d("活动数：%s", ACTIVITIES.size());
+        for (int i = 0; i < ACTIVITIES.size(); i++) {
+            Timber.d("概览：%s", ACTIVITIES.get(i).getClass().getSimpleName());
+        }
+    }
+
+    /**
+     * 从堆栈去 Activity
+     *
+     * @param activity Activity
+     */
+    public void removeActivity(Activity activity) {
+        ACTIVITIES.remove(activity);
+        Timber.d("去除：%s", activity.getClass().getSimpleName());
+        Timber.d("活动数：%s", ACTIVITIES.size());
+        for (int i = 0; i < ACTIVITIES.size(); i++) {
+            Timber.d("概览：%s", ACTIVITIES.get(i).getClass().getSimpleName());
+        }
+    }
+
+    /**
+     * 当前 Activity 名
+     * <p>
+     * info.topActivity.getShortClassName() Activity 名
+     * info.topActivity.getClassName() 类名
+     * info.topActivity.getPackageName() 包名
+     * info.topActivity.getClass() 类实例
+     *
+     * @param context 上下文
+     * @return 当前 Activity 名
+     */
+    public String getCurrentRunningActivityName(@NotNull Context context) {
+        ActivityManager activityManager = (ActivityManager) context.getApplicationContext().getSystemService(ACTIVITY_SERVICE);
+        ActivityManager.RunningTaskInfo runningTaskInfo = ((null != activityManager) ? activityManager.getRunningTasks(1).get(0) : null);
+        String currentRunningActivityName = ((null != runningTaskInfo) && (null != runningTaskInfo.topActivity)) ? runningTaskInfo.topActivity.getShortClassName() : null;
+        Timber.d("当前活动名：%s", currentRunningActivityName);
+        return currentRunningActivityName;
+    }
+
+    /**
+     * 栈顶 Activity 实例
+     *
+     * @return 栈顶 Activity 实例
+     */
+    public @Nullable Activity getTopActivityInstance() {
+        Activity topActivityInstance;
+        synchronized (ACTIVITIES) {
+            final int size = (ACTIVITIES.size() - 1);
+            if (size < 0) {
+                return null;
+            }
+            topActivityInstance = ACTIVITIES.get(size);
+        }
+        return topActivityInstance;
+    }
+
+    /**
+     * 结束指定 Activity
+     *
+     * @param activity Activity
+     */
+    private void finishActivity(Activity activity) {
+        if (ACTIVITIES.isEmpty()) {
+            return;
+        }
+        if (null != activity) {
+            Timber.d("结束：%s", activity.getClass().getSimpleName());
+            ACTIVITIES.remove(activity);
+            activity.finish();
+        }
+    }
+
+    /**
+     * 结束指定类名 Activity
+     *
+     * @param cls Class<?>
+     */
+    public void finishActivity(Class<?> cls) {
+        if (ACTIVITIES.isEmpty()) {
+            return;
+        }
+        for (Activity activity : ACTIVITIES) {
+            if (activity.getClass().equals(cls)) {
+                finishActivity(activity);
+            }
+        }
+    }
+
+    /**
+     * 结束所有 Activity
+     */
+    private void finishAllActivity() {
+        for (Activity activity : ACTIVITIES) {
+            activity.finish();
+        }
+        ACTIVITIES.clear();
+    }
+
+    /**
+     * 双击退出
+     *
+     * @param exitHint 退出提示
+     */
+    public void twoClickToExit(String exitHint) {
+        if ((CurrentTimeMillisClock.getInstance().now() - touchDownTime) < WAIT_TIME) {
+            appExit();
+        } else {
+            touchDownTime = CurrentTimeMillisClock.getInstance().now();
+            ToastKit.showShort(exitHint);
+        }
+    }
+
+    /**
+     * 退应用
+     */
+    public void appExit() {
+        try {
+            finishAllActivity();
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+    }
+
+    private static final class InstanceHolder {
+        static final ActivitySuperviseManager INSTANCE = new ActivitySuperviseManager();
+    }
+}
---
+++ core/src/main/java/util/cache/CacheManager.java
@@ -0,0 +1,120 @@
+package util.cache;
+
+import android.content.Context;
+import android.os.Environment;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import timber.log.Timber;
+
+/**
+ * 缓存管理器
+ *
+ * @author 郑少鹏
+ * @date 2018/11/6
+ */
+public class CacheManager {
+    /**
+     * 全缓存大小
+     *
+     * @param context 上下文
+     * @return 全缓存大小
+     */
+    public static @NotNull String totalCacheSize(@NotNull Context context) {
+        long cacheSize = folderSize(context.getApplicationContext().getCacheDir());
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            cacheSize += folderSize(context.getApplicationContext().getExternalCacheDir());
+        }
+        return formatSize(cacheSize);
+    }
+
+    /**
+     * 清全缓存
+     *
+     * @param context 上下文
+     */
+    public static void clearAllCache(@NotNull Context context) {
+        deleteDir(context.getApplicationContext().getCacheDir());
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            deleteDir(context.getApplicationContext().getExternalCacheDir());
+        }
+    }
+
+    private static boolean deleteDir(File dir) {
+        if ((null != dir) && dir.isDirectory()) {
+            String[] children = dir.list();
+            if (null != children) {
+                for (String aChildren : children) {
+                    boolean success = deleteDir(new File(dir, aChildren));
+                    if (!success) {
+                        return false;
+                    }
+                }
+            }
+        }
+        if (null != dir) {
+            return dir.delete();
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 目录大小
+     * <p>
+     * Context.getExternalFilesDir() --> SDCard/Android/data/应用包名/files/ 目录（通放长存数据）
+     * Context.getExternalCacheDir() --> SDCard/Android/data/应用包名/cache/ 目录（通放临缓数据）
+     *
+     * @param file 文件
+     * @return 目录大小
+     */
+    private static long folderSize(File file) {
+        long size = 0;
+        try {
+            File[] fileList = file.listFiles();
+            if (null != fileList) {
+                for (File aFileList : fileList) {
+                    // 下面还有文件
+                    size = (aFileList.isDirectory() ? (size + folderSize(aFileList)) : (size + aFileList.length()));
+                }
+            }
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+        return size;
+    }
+
+    /**
+     * 格式化大小
+     *
+     * @param size 大小
+     * @return 格式化大小
+     */
+    private static @NotNull String formatSize(double size) {
+        double kiloByte = (size / 1024);
+        if (kiloByte < 1) {
+            return "0K";
+        }
+        double megaByte = (kiloByte / 1024);
+        if (megaByte < 1) {
+            BigDecimal result1 = BigDecimal.valueOf(kiloByte);
+            return result1.setScale(2, RoundingMode.HALF_UP).toPlainString() + "KB";
+        }
+        double gigaByte = (megaByte / 1024);
+        if (gigaByte < 1) {
+            BigDecimal result2 = new BigDecimal(megaByte);
+            return result2.setScale(2, RoundingMode.HALF_UP).toPlainString() + "MB";
+        }
+        double teraBytes = (gigaByte / 1024);
+        if (teraBytes < 1) {
+            BigDecimal result3 = new BigDecimal(gigaByte);
+            return result3.setScale(2, RoundingMode.HALF_UP).toPlainString() + "GB";
+        }
+        BigDecimal result4 = new BigDecimal(teraBytes);
+        return result4.setScale(2, RoundingMode.HALF_UP).toPlainString() + "TB";
+    }
+}
