a18131ac643a92402992f621cb66de6ac7661120
snpmyn <snpmyn@126.com>
2025-08-01 10:36:12 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/value/Magic.java
@@ -0,0 +1,15 @@
+package com.zsp.today.value;
+
+/**
+ * Created on 2020/12/10
+ *
+ * @author zsp
+ * @desc 魔法值
+ */
+public class Magic {
+    /**
+     * String
+     */
+    public static final String STRING_SPLASH_ACTIVITY = "SplashActivity";
+    public static final String STRING_ZERO_K = "0K";
+}
