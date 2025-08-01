47158fafd78066d1e4bac2977fdcf29c2cc9612d
snpmyn <snpmyn@126.com>
2025-08-01 22:56:49 +0800

    提交

--- core/src/main/java/fragmentation/support/SupportActivity.java
+++ core/src/main/java/fragmentation/support/SupportActivity.java
@@ -77,11 +77,13 @@ public class SupportActivity extends AppCompatActivity implements ISupportActivi
     /**
      * onBackPressed
      * <p>
+     * 调父类构造方法会导致返回顺序紊乱
+     * super.onBackPressed();
+     * <p>
      * 不建复写该方法，{@link #onBackPressedSupport} 替。
      */
     @Override
     final public void onBackPressed() {
-        super.onBackPressed();
         supportActivityDelegate.onBackPressed();
     }
 
