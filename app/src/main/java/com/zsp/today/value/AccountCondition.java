e3abfa93d3acf912001ddbda820922ecbc190020
snpmyn <snpmyn@126.com>
2025-08-01 10:35:47 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/value/AccountCondition.java
@@ -0,0 +1,16 @@
+package com.zsp.today.value;
+
+/**
+ * Created on 2022/6/28
+ *
+ * @author zsp
+ * @desc 账目条件
+ */
+public class AccountCondition {
+    public static String ACCOUNT_PHONE_NUMBER = "phoneNumber = ?";
+    public static String ACCOUNT_PHONE_NUMBER_AND_YEAR = "phoneNumber = ? and year = ?";
+    public static String ACCOUNT_PHONE_NUMBER_AND_YEAR_AND_MONTH = "phoneNumber = ? and year = ? and month = ?";
+    public static String ACCOUNT_PHONE_NUMBER_AND_DATE = "phoneNumber = ? and date = ?";
+    public static String ACCOUNT_PHONE_NUMBER_AND_DATE_AND_CATEGORY = "phoneNumber = ? and date = ? and category = ?";
+    public static String ACCOUNT_PHONE_NUMBER_AND_DATE_AND_CATEGORY_AND_AMOUNT = "phoneNumber = ? and date = ? and category = ? and amount = ?";
+}
---
+++ app/src/main/java/com/zsp/today/value/AccountConstant.java
@@ -0,0 +1,22 @@
+package com.zsp.today.value;
+
+/**
+ * Created on 2022/6/20
+ *
+ * @author zsp
+ * @desc 账目常量
+ */
+public class AccountConstant {
+    /**
+     * 账目主页（账目传输）
+     */
+    public static final String ACCOUNT_HOME_ACTIVITY_$_ACCOUNT_TRANSFER_BEAN = "AccountHomeActivityAccountTransferBean";
+    /**
+     * 账目二页（账目传输）
+     */
+    public static final String ACCOUNT_SECOND_ACTIVITY_$_ACCOUNT_TRANSFER_BEAN = "AccountSecondActivityAccountTransferBean";
+    /**
+     * 账目详情页（账目传输）
+     */
+    public static final String ACCOUNT_DETAIL_ACTIVITY_$_ACCOUNT_TRANSFER_BEAN = "AccountDetailActivityAccountTransferBean";
+}
