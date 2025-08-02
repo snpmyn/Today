98a15469cc6437b1bb74da46d76d0fdc7d9844d4
snpmyn <snpmyn@126.com>
2025-08-02 13:11:16 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/value/FunctionCondition.java
@@ -0,0 +1,12 @@
+package com.zsp.today.value;
+
+/**
+ * Created on 2022/6/28
+ *
+ * @author zsp
+ * @desc 功能条件
+ */
+public class FunctionCondition {
+    public static String FUNCTION_FUNCTION_ID = "functionId = ?";
+    public static String FUNCTION_FUNCTION_SHOW = "functionShow = ?";
+}
