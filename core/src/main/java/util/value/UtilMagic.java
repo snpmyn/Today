90a3d035f5a3123155bdf63fcf15f111cccd6dd0
snpmyn <snpmyn@126.com>
2025-08-02 14:50:48 +0800

    提交

---
+++ core/src/main/java/util/value/UtilMagic.java
@@ -0,0 +1,60 @@
+package util.value;
+
+/**
+ * Created on 2018/3/5.
+ *
+ * @author 郑少鹏
+ * @desc 工具类魔法值
+ */
+public class UtilMagic {
+    /**
+     * int
+     */
+    public static final int INT_TWO = 2;
+    public static final int INT_FOUR = 4;
+    public static final int INT_FIVE = 5;
+    public static final int INT_EIGHT = 8;
+    public static final int INT_TEN = 10;
+    public static final int INT_TWELVE = 12;
+    public static final int INT_FOURTEEN = 14;
+    public static final int INT_FIFTEEN = 15;
+    public static final int INT_SIXTEEN = 16;
+    public static final int INT_EIGHTEEN = 18;
+    public static final int INT_NINETEEN = 19;
+    public static final int INT_TWO_HUNDRED = 200;
+    /**
+     * String
+     */
+    public static final String STRING_DOT = ".";
+    public static final String STRING_DOT_DOC = ".doc";
+    public static final String STRING_DOT_DOCX = ".docx";
+    public static final String STRING_DOT_PDF = ".pdf";
+    public static final String STRING_DOT_PPT = ".ppt";
+    public static final String STRING_DOT_PPTX = ".pptx";
+    public static final String STRING_DOT_XLS = ".xls";
+    public static final String STRING_DOT_XLSX = ".xlsx";
+    public static final String STRING_DOT_ZIP = ".zip";
+    public static final String STRING_DOT_RAR = ".rar";
+    public static final String STRING_DOT_RTF = ".rtf";
+    public static final String STRING_DOT_WAV = ".wav";
+    public static final String STRING_DOT_MP3 = ".mp3";
+    public static final String STRING_DOT_GIF = ".gif";
+    public static final String STRING_DOT_JPG = ".jpg";
+    public static final String STRING_DOT_JPEG = ".jpeg";
+    public static final String STRING_DOT_PNG = ".png";
+    public static final String STRING_DOT_TXT = ".txt";
+    public static final String STRING_DOT_3GP = ".3gp";
+    public static final String STRING_DOT_MPG = ".mpg";
+    public static final String STRING_DOT_MPEG = ".mpeg";
+    public static final String STRING_DOT_MPE = ".mpe";
+    public static final String STRING_DOT_MP4 = ".mp4";
+    public static final String STRING_DOT_AVI = ".avi";
+    public static final String STRING_BACKSLASH = "/";
+    public static final String STRING_FILE = "file";
+    public static final String STRING_CONTENT = "content";
+    public static final String STRING_PRIMARY = "primary";
+    public static final String STRING_RAW_COLON = "raw:";
+    public static final String STRING_IMAGE = "image";
+    public static final String STRING_VIDEO = "video";
+    public static final String STRING_AUDIO = "audio";
+}
