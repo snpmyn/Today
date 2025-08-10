e73fa36dec2b532ddf261351822900c3335c9167
snpmyn <snpmyn@126.com>
2025-08-10 21:08:51 +0800

    提交

---
+++ core/src/main/java/widget/carousel/CarouselViewHolder.java
@@ -0,0 +1,43 @@
+package widget.carousel;
+
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.zsp.core.R;
+
+/**
+ * @decs: 轮播视图持有器
+ * @author: 郑少鹏
+ * @date: 2025/8/10 16:36
+ * @version: v 1.0
+ */
+public class CarouselViewHolder extends RecyclerView.ViewHolder {
+    private final ImageView imageView;
+    private final CarouselListener carouselListener;
+
+    /**
+     * constructor
+     *
+     * @param itemView         条目视图
+     * @param carouselListener 轮播监听
+     */
+    public CarouselViewHolder(View itemView, CarouselListener carouselListener) {
+        super(itemView);
+        this.imageView = itemView.findViewById(R.id.carouselItemIv);
+        this.carouselListener = carouselListener;
+    }
+
+    /**
+     * 绑定
+     *
+     * @param carouselItem 轮播条目
+     */
+    public void bind(@NonNull CarouselItem carouselItem) {
+        Glide.with(imageView.getContext()).load(carouselItem.getDrawableResId()).centerCrop().into(imageView);
+        itemView.setOnClickListener(v -> carouselListener.onItemClick(carouselItem, getAdapterPosition()));
+    }
+}
