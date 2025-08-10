7a7ab829a6059d9aba7ec34ca293bd694a93ba57
snpmyn <snpmyn@126.com>
2025-08-10 20:50:42 +0800

    提交

---
+++ core/src/main/java/widget/dialog/customdialog/CustomDialog.java
@@ -0,0 +1,65 @@
+package widget.dialog.customdialog;
+
+import android.os.Bundle;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.jetbrains.annotations.Contract;
+
+/**
+ * @decs: CustomDialog
+ * @author: 郑少鹏
+ * @date: 2018/4/4 13:43
+ */
+public class CustomDialog extends BaseDialog {
+    private BaseViewConvertListener baseViewConvertListener;
+
+    @NonNull
+    @Contract(" -> new")
+    public static CustomDialog init() {
+        return new CustomDialog();
+    }
+
+    @Override
+    public int intLayoutId() {
+        return layoutId;
+    }
+
+    @Override
+    public void convertView(ViewHolder holder, BaseDialog dialog) {
+        if (null != baseViewConvertListener) {
+            baseViewConvertListener.convertView(holder, dialog);
+        }
+    }
+
+    public CustomDialog setLayoutId(@LayoutRes int layoutId) {
+        this.layoutId = layoutId;
+        return this;
+    }
+
+    public CustomDialog setBaseViewConvertListener(BaseViewConvertListener baseViewConvertListener) {
+        this.baseViewConvertListener = baseViewConvertListener;
+        return this;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (null != savedInstanceState) {
+            baseViewConvertListener = savedInstanceState.getParcelable("listener");
+        }
+    }
+
+    /**
+     * 保存接口
+     *
+     * @param outState outState
+     */
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable("listener", baseViewConvertListener);
+    }
+}
---
+++ core/src/main/java/widget/dialog/customdialog/ViewHolder.java
@@ -0,0 +1,74 @@
+package widget.dialog.customdialog;
+
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import org.jetbrains.annotations.Contract;
+
+/**
+ * @decs: ViewHolder
+ * @author: 郑少鹏
+ * @date: 2018/4/4 13:52
+ */
+public class ViewHolder {
+    private final SparseArray<View> views;
+    private final View convertView;
+
+    private ViewHolder(View view) {
+        convertView = view;
+        views = new SparseArray<>();
+    }
+
+    @NonNull
+    @Contract("_ -> new")
+    static ViewHolder create(View view) {
+        return new ViewHolder(view);
+    }
+
+    public <T extends View> T getView(int viewId) {
+        View view = views.get(viewId);
+        if (view == null) {
+            view = convertView.findViewById(viewId);
+            views.put(viewId, view);
+        }
+        //noinspection unchecked
+        return (T) view;
+    }
+
+    public View getConvertView() {
+        return convertView;
+    }
+
+    public void setText(int viewId, String text) {
+        TextView textView = getView(viewId);
+        textView.setText(text);
+    }
+
+    public void setText(int viewId, int textId) {
+        TextView textView = getView(viewId);
+        textView.setText(textId);
+    }
+
+    public void setTextColor(int viewId, int colorId) {
+        TextView textView = getView(viewId);
+        textView.setTextColor(colorId);
+    }
+
+    public void setOnClickListener(int viewId, View.OnClickListener clickListener) {
+        View view = getView(viewId);
+        view.setOnClickListener(clickListener);
+    }
+
+    public void setBackgroundResource(int viewId, int resId) {
+        View view = getView(viewId);
+        view.setBackgroundResource(resId);
+    }
+
+    public void setBackgroundColor(int viewId, int colorId) {
+        View view = getView(viewId);
+        view.setBackgroundColor(colorId);
+    }
+}
