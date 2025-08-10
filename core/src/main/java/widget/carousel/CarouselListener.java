a8ab27b2a1684a97f894b5b007fc21353c8a2cee
snpmyn <snpmyn@126.com>
2025-08-10 21:07:16 +0800

    提交

---
+++ core/src/main/java/widget/carousel/CarouselListener.java
@@ -0,0 +1,17 @@
+package widget.carousel;
+
+/**
+ * @decs: 轮播监听
+ * @author: 郑少鹏
+ * @date: 2025/8/10 16:23
+ * @version: v 1.0
+ */
+public interface CarouselListener {
+    /**
+     * 短点
+     *
+     * @param carouselItem 轮播条目
+     * @param position     位置
+     */
+    void onItemClick(CarouselItem carouselItem, int position);
+}
