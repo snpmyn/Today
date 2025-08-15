31e4164b8f4429b95d10abbfa82bdc09e683bd80
snpmyn <snpmyn@126.com>
2025-08-16 01:13:27 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/value/RxBusConstant.java
@@ -0,0 +1,46 @@
+package com.zsp.today.value;
+
+/**
+ * Created on 2020/12/30
+ *
+ * @author zsp
+ * @desc RxBus 常量
+ */
+public class RxBusConstant {
+    /**
+     * 主页（底导航视图）
+     */
+    public static final String MAIN_ACTIVITY_$_BOTTOM_NAVIGATION_VIEW = "MainActivityBottomNavigationView";
+    /**
+     * 主页（隐底导航视图码）
+     */
+    public static final int MAIN_ACTIVITY_$_HIDE_BOTTOM_NAVIGATION_VIEW_CODE = 0x01;
+    /**
+     * 主页（显底导航视图码）
+     */
+    public static final int MAIN_ACTIVITY_$_SHOW_BOTTOM_NAVIGATION_VIEW_CODE = 0x02;
+    /**
+     * 首页子碎片（刷新菜单）
+     */
+    public static final String HOME_PAGE_CHILD_FRAGMENT_$_REFRESH_MENU = "HomePageChildFragmentRefreshMenu";
+    /**
+     * 首页子碎片（刷新菜单码）
+     */
+    public static final int HOME_PAGE_CHILD_FRAGMENT_$_REFRESH_MENU_CODE = 0x03;
+    /**
+     * 账目主页和二页（刷新账目）
+     */
+    public static final String ACCOUNT_HOME_ACTIVITY_AND_SECOND_ACTIVITY_$_REFRESH_ACCOUNT = "AccountHomeActivityAndSecondActivityRefreshAccount";
+    /**
+     * 账目主页和二页（刷新账目码）
+     */
+    public static final int ACCOUNT_HOME_ACTIVITY_AND_SECOND_ACTIVITY_$_REFRESH_ACCOUNT_CODE = 0x04;
+    /**
+     * 账目详情页（刷新账目）
+     */
+    public static final String ACCOUNT_DETAIL_ACTIVITY_$_REFRESH_ACCOUNT = "AccountDetailActivityRefreshAccount";
+    /**
+     * 账目详情页（刷新账目码）
+     */
+    public static final int ACCOUNT_DETAIL_ACTIVITY_$_REFRESH_ACCOUNT_CODE = 0x05;
+}
