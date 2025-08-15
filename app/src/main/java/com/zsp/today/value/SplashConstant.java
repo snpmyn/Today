b6341249975240e1553ae8cee33cf9b1b01d20d3
snpmyn <snpmyn@126.com>
2025-08-15 22:02:44 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/value/SplashConstant.java
@@ -0,0 +1,14 @@
+package com.zsp.today.value;
+
+/**
+ * Created on 2022/6/20
+ *
+ * @author zsp
+ * @desc 闪屏常量
+ */
+public class SplashConstant {
+    /**
+     * 闪屏动画主页碎片（资源名）
+     */
+    public static final String SPLASH_ANIMATION_HOME_FRAGMENT_$_RES_NAME = "SplashAnimationHomeFragmentResName";
+}
