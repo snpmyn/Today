6a9d6a408b7dd141f211f8a1a087e0d4d491ee25
snpmyn <snpmyn@126.com>
2025-08-27 01:03:03 +0800

    提交

---
+++ core/src/main/java/util/handler/HandlerKit.java
@@ -0,0 +1,27 @@
+package util.handler;
+
+import android.os.Handler;
+import android.os.Looper;
+
+/**
+ * Created on 2025/8/25.
+ *
+ * @author 郑少鹏
+ * @desc Handler 配套元件
+ */
+public class HandlerKit extends Handler {
+    public static HandlerKit getInstance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    /**
+     * Use the provided {@link Looper} instead of the default one.
+     */
+    public HandlerKit() {
+        super(Looper.getMainLooper());
+    }
+
+    private static final class InstanceHolder {
+        static final HandlerKit INSTANCE = new HandlerKit();
+    }
+}
