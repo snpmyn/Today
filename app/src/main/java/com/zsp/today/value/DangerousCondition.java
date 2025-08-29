2f37b69ad9c6281188f758b3a904ceafc0f1ada3
snpmyn <snpmyn@126.com>
2025-08-29 21:39:01 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/value/DangerousCondition.java
@@ -0,0 +1,11 @@
+package com.zsp.today.value;
+
+/**
+ * Created on 2025/8/29.
+ *
+ * @author 郑少鹏
+ * @desc 险情常量
+ */
+public class DangerousCondition {
+    public static String DANGEROUS_PHONE_NUMBER = "phoneNumber = ?";
+}
