85012fec72def163632cd9adb4b9864b49bf97fc
snpmyn <snpmyn@126.com>
2025-09-01 14:23:08 +0800

    提交

---
+++ bdmap/src/main/java/com/zsp/bdmap/value/BdMapConstant.java
@@ -0,0 +1,14 @@
+package com.zsp.bdmap.value;
+
+/**
+ * Created on 2025/8/31.
+ *
+ * @author 郑少鹏
+ * @desc 百度地图常量
+ */
+public class BdMapConstant {
+    /**
+     * 百度地图定位
+     */
+    public static final String BD_MAP_$_LOCATION = "BdMapLocation";
+}
