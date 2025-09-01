15df646f425c2fdf632cee7f30cf1b186956d179
snpmyn <snpmyn@126.com>
2025-09-01 14:36:57 +0800

    提交

---
+++ core/src/main/java/widget/location/value/LocationConstant.java
@@ -0,0 +1,14 @@
+package widget.location.value;
+
+/**
+ * Created on 2025/9/1.
+ *
+ * @author 郑少鹏
+ * @desc 定位常量
+ */
+public class LocationConstant {
+    /**
+     * 定位信息
+     */
+    public static final String LOCATION_INFO = "LocationInfo";
+}
