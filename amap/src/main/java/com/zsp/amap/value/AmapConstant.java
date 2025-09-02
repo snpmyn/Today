669ef8b97c2b8c032e4b121f9b7af8bbb37faf8d
snpmyn <snpmyn@126.com>
2025-09-02 15:28:47 +0800

    提交

---
+++ amap/src/main/java/com/zsp/amap/value/AmapConstant.java
@@ -0,0 +1,14 @@
+package com.zsp.amap.value;
+
+/**
+ * Created on 2025/9/1.
+ *
+ * @author 郑少鹏
+ * @desc 高德地图常量
+ */
+public class AmapConstant {
+    /**
+     * 高德地图定位
+     */
+    public static final String AMAP_$_LOCATION = "AmapLocation";
+}
