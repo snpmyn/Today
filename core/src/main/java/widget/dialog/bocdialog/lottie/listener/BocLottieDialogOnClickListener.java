c6ea766ab5ce4111169b919ab6d7a761b431119b
snpmyn <snpmyn@126.com>
2025-09-04 16:29:33 +0800

    提交

---
+++ core/src/main/java/widget/dialog/bocdialog/lottie/listener/BocLottieDialogOnClickListener.java
@@ -0,0 +1,14 @@
+package widget.dialog.bocdialog.lottie.listener;
+
+/**
+ * Created on 2025/9/3.
+ *
+ * @author 郑少鹏
+ * @desc BOC Lottie 对话框点击监听
+ */
+public interface BocLottieDialogOnClickListener {
+    /**
+     * 点击
+     */
+    void onClick();
+}
