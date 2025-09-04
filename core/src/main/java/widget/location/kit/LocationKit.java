ebd799431a98139bb1e8a40d81292fbdeae4abe2
snpmyn <snpmyn@126.com>
2025-09-04 16:32:16 +0800

    提交

---
+++ core/src/main/java/widget/location/kit/LocationKit.java
@@ -0,0 +1,113 @@
+package widget.location.kit;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.zsp.core.R;
+
+import java.util.List;
+
+import util.mmkv.MmkvKit;
+import widget.location.listener.LocationKitListener;
+import widget.location.value.LocationConstant;
+import widget.permissionx.kit.PermissionxKit;
+import widget.permissionx.listener.PermissionxKitListener;
+import widget.toast.ToastKit;
+
+/**
+ * Created on 2019/4/24.
+ *
+ * @author 郑少鹏
+ * @desc 定位配套原件
+ */
+public class LocationKit {
+    /**
+     * 定位管理器
+     */
+    private LocationManager locationManager;
+    /**
+     * 定位监听
+     */
+    private LocationListener locationListener;
+
+    public static LocationKit getInstance() {
+        return InstanceHolder.INSTANCE;
+    }
+
+    /**
+     * 执行
+     *
+     * @param appCompatActivity   活动
+     * @param locationKitListener 定位配套原件监听
+     */
+    public void execute(AppCompatActivity appCompatActivity, LocationKitListener locationKitListener) {
+        PermissionxKit.execute(appCompatActivity, List.of(Manifest.permission.ACCESS_FINE_LOCATION), R.string.getLocationAreBasedOnThePermission, R.string.youNeedToAllowNecessaryPermissionInSettingManually, R.string.agree, R.string.refuse, new PermissionxKitListener() {
+            @Override
+            public void allGranted() {
+                startLocation(appCompatActivity, locationKitListener);
+            }
+
+            @Override
+            public void allGrantedContrary() {
+
+            }
+        });
+    }
+
+    /**
+     * 开始定位
+     *
+     * @param appCompatActivity   活动
+     * @param locationKitListener 定位配套原件监听
+     */
+    private void startLocation(@NonNull AppCompatActivity appCompatActivity, LocationKitListener locationKitListener) {
+        locationManager = (LocationManager) appCompatActivity.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = location -> {
+            // 经度
+            double longitude = location.getLongitude();
+            // 纬度
+            double latitude = location.getLatitude();
+            // 精度
+            float accuracy = location.getAccuracy();
+            String locationInfo = String.format(appCompatActivity.getString(R.string.formatLocation), longitude, latitude, accuracy);
+            MmkvKit.defaultMmkv().encode(LocationConstant.LOCATION_INFO, locationInfo);
+            if (null != locationKitListener) {
+                locationKitListener.onLocationChanged(locationInfo);
+            }
+        };
+        // 是否允许 GPS 定位
+        boolean areGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        // 是否允许网络定位
+        boolean areNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        if (areGPSEnabled) {
+            // 权限在前面已申请
+            // 此处因不设置报错
+            if (ActivityCompat.checkSelfPermission(appCompatActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(appCompatActivity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10, locationListener);
+        } else if (areNetworkEnabled) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 10, locationListener);
+        } else {
+            ToastKit.showShort(appCompatActivity.getString(R.string.pleaseTurnOnLocationServices));
+        }
+    }
+
+    /**
+     * 移除更新
+     */
+    public void removeUpdates() {
+        locationManager.removeUpdates(locationListener);
+    }
+
+    private static final class InstanceHolder {
+        static final LocationKit INSTANCE = new LocationKit();
+    }
+}
