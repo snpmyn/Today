73dbaef0649d81e3abb5d92b2fb532a7eb677b6d
snpmyn <snpmyn@126.com>
2025-09-05 14:58:57 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/value/DangerousConstant.java
@@ -0,0 +1,14 @@
+package com.zsp.today.value;
+
+/**
+ * Created on 2025/9/4.
+ *
+ * @author 郑少鹏
+ * @desc 险情常量
+ */
+public class DangerousConstant {
+    /**
+     * 险情页（使用指南）
+     */
+    public static final String DANGEROUS_ACTIVITY_$_USE_GUIDE = "DangerousActivityUseGuide";
+}
