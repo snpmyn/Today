15dacad8b39d0bc96304a888b565c373eafa42c4
snpmyn <snpmyn@126.com>
2025-09-06 14:42:32 +0800

    提交

--- core/src/main/java/widget/sms/broadcastreceiver/SmsBroadcastReceiver.java
+++ core/src/main/java/widget/sms/broadcastreceiver/SmsBroadcastReceiver.java
@@ -16,6 +16,21 @@ import widget.sms.kit.SmsKit;
  *
  * @author 郑少鹏
  * @desc 短信广播接收器
+ * <p>
+ * 在 Android 中，发送短信时，响应码为 0 通常表示发送成功。
+ * 但是，这个响应码并不是标准的短信发送成功标志，而是 Android 系统内部的返回值。
+ * <p>
+ * 使用 SmsManager 发送短信：如果你是通过 SmsManager 发送短信，那么通常会通过 SentIntent 来接收结果。
+ * 在这个 SentIntent 中，resultCode 为 Activity.RESULT_OK 表示短信发送成功。
+ * 而 0 可能表示没有明确的错误，或者是默认的成功标志，具体还需查看具体文档。
+ * <p>
+ * 通过 SmsManager 发送短信并使用广播接收结果：Android 中的 SmsManager 可以通过 BroadcastReceiver 发送一个发送结果的广播。
+ * 此时，结果码 0 可能只是系统内部返回的一个默认值，并不代表失败。
+ * <p>
+ * 网络问题或运营商的限制：有时即使代码返回 0，短信可能由于网络问题或运营商的限制而未能真正发出去。
+ * 在这种情况下，你应该检查其他的网络连接和配置，确保短信能够顺利发送。
+ * <p>
+ * 总结：0 通常表示成功，但最好结合具体的应用上下文、短信发送的 API 以及设备/运营商配置来确认是否真正成功。
  */
 public class SmsBroadcastReceiver extends BroadcastReceiver {
     /**
@@ -50,6 +65,7 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
         if (null != intent.getAction()) {
             if (intent.getAction().equals(SmsKit.SMS_SEND_ACTION)) {
                 switch (getResultCode()) {
+                    case 0:
                     case Activity.RESULT_OK:
                         // 短信发送成功
                         smsBroadcastReceiverSendListener.sendResultOk();
@@ -75,6 +91,7 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
                 }
             } else if (intent.getAction().equals(SmsKit.SMS_DELIVER_ACTION)) {
                 switch (getResultCode()) {
+                    case 0:
                     case Activity.RESULT_OK:
                         // 短信已送达
                         smsBroadcastReceiverDeliverListener.deliverResultOk();
