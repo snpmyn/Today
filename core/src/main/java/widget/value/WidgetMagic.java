b72110ed7688a458fdd2e653776fc642aab536a2
snpmyn <snpmyn@126.com>
2025-09-06 11:54:56 +0800

    提交

---
+++ core/src/main/java/widget/value/WidgetMagic.java
@@ -0,0 +1,26 @@
+package widget.value;
+
+/**
+ * Created on 2018/3/5.
+ *
+ * @author 郑少鹏
+ * @desc 组件魔法值
+ */
+public class WidgetMagic {
+    /**
+     * int
+     */
+    public static final int INT_TWO = 2;
+    public static final int INT_THREE = 3;
+    /**
+     * String
+     */
+    public static final String STRING_ONE_SPACE = " ";
+    public static final String STRING_ONE_OTHER_SPACE = "　";
+    public static final String STRING_TWO_SPACE = "  ";
+    public static final String STRING_TWO_OTHER_SPACE = "　　";
+    public static final String STRING_THREE_SPACE = "   ";
+    public static final String STRING_THREE_OTHER_SPACE = "　　　";
+    public static final String STRING_FOUR_SPACE = "    ";
+    public static final String STRING_NEW_LINE = "\n";
+}
