f4f6eaa5b2f5bd5d851ded23dbc2ed6de52a4d88
snpmyn <snpmyn@126.com>
2025-09-08 18:07:44 +0800

    提交

--- app/src/main/java/com/zsp/today/basic/kit/RestoreAccountDataBaseTableKit.java
+++ app/src/main/java/com/zsp/today/basic/kit/RestoreAccountDataBaseTableKit.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.zsp.today.R;
+import com.zsp.today.basic.restore.RestoreConstant;
 import com.zsp.today.basic.value.Folder;
 import com.zsp.today.module.account.database.AccountDataBaseTable;
 
@@ -18,9 +19,9 @@ import litepal.kit.LitePalKit;
 import util.file.FileUtils;
 import util.json.JsonTransform;
 import util.list.ListUtils;
+import util.mmkv.MmkvKit;
 import util.timer.TimerKit;
 import widget.dialog.bocdialog.kit.BocDialogKit;
-import widget.dialog.bocdialog.lottie.BocLottieCommonDialog;
 import widget.dialog.bocdialog.lottie.bean.BocLottieDialogEnum;
 
 /**
@@ -40,13 +41,12 @@ public class RestoreAccountDataBaseTableKit {
      * @param appCompatActivity 活动
      */
     public void restoreAccountDataBaseTable(AppCompatActivity appCompatActivity) {
-        if (LitePalKit.getInstance().count(AccountDataBaseTable.class) != 0) {
-            // 账目数据库表有数据
+        if (MmkvKit.defaultMmkv().decodeBool(RestoreConstant.RESTORE_$_ACCOUNT_DATA_BASE_TABLE)) {
             // 预存或恢复功能数据库表
-            RestoreFunctionDataBaseTableKit.getInstance().preStoreOrRestoreFunctionDataBaseTable(appCompatActivity, null);
+            RestoreFunctionDataBaseTableKit.getInstance().preStoreOrRestoreFunctionDataBaseTable(appCompatActivity);
             return;
         }
-        BocLottieCommonDialog bocLottieCommonDialog = BocDialogKit.getInstance(appCompatActivity).bocLottieCommonDialogTwo(BocLottieDialogEnum.LOADING_ONE, appCompatActivity.getString(R.string.restoreAccount), ValueAnimator.INFINITE, null, null);
+        RestoreKit.getInstance().bocLottieCommonDialog = BocDialogKit.getInstance(appCompatActivity).bocLottieCommonDialogTwo(BocLottieDialogEnum.LOADING_ONE, appCompatActivity.getString(R.string.restoreAccount), ValueAnimator.INFINITE, null, null);
         // 账目数据库表文件地址
         String accountDataBaseTableFilePath = Folder.EXTERNAL_BACKUP + File.separator + AccountDataBaseTable.class.getSimpleName() + ".json";
         if (FileUtils.areFileExistByPath(accountDataBaseTableFilePath)) {
@@ -57,7 +57,7 @@ public class RestoreAccountDataBaseTableKit {
             if (ListUtils.listIsEmpty(accountDataBaseTables)) {
                 // 无账目数据库表文件数据
                 // 下一步
-                next(appCompatActivity, bocLottieCommonDialog, BocLottieDialogEnum.EMPTY_ONE, appCompatActivity.getString(R.string.noAccountAvailable));
+                next(appCompatActivity, BocLottieDialogEnum.EMPTY_ONE, appCompatActivity.getString(R.string.noAccountAvailable));
             } else {
                 // 有账目数据库表文件数据
                 // 转化存储恢复
@@ -67,28 +67,28 @@ public class RestoreAccountDataBaseTableKit {
                 }
                 if (LitePalKit.getInstance().multiSave(accountDataBaseTableList)) {
                     // 下一步
-                    next(appCompatActivity, bocLottieCommonDialog, BocLottieDialogEnum.SUCCESS_ONE, appCompatActivity.getString(R.string.accountHasBeenRestored));
+                    next(appCompatActivity, BocLottieDialogEnum.SUCCESS_ONE, appCompatActivity.getString(R.string.accountHasBeenRestored));
                 }
             }
         } else {
             // 账目数据库表文件不存在
             // 下一步
-            next(appCompatActivity, bocLottieCommonDialog, BocLottieDialogEnum.EMPTY_ONE, appCompatActivity.getString(R.string.noAccountAvailable));
+            next(appCompatActivity, BocLottieDialogEnum.EMPTY_ONE, appCompatActivity.getString(R.string.noAccountAvailable));
         }
     }
 
     /**
      * 下一步
      *
-     * @param appCompatActivity     活动
-     * @param bocLottieCommonDialog BOC Lottie 普通对话框
-     * @param bocLottieDialogEnum   BOC Lottie 对话框枚举
-     * @param hint                  提示
+     * @param appCompatActivity   活动
+     * @param bocLottieDialogEnum BOC Lottie 对话框枚举
+     * @param hint                提示
      */
-    private void next(AppCompatActivity appCompatActivity, BocLottieCommonDialog bocLottieCommonDialog, BocLottieDialogEnum bocLottieDialogEnum, String hint) {
-        TimerKit.getInstance().execute(appCompatActivity, RestoreKit.getInstance().delay, () -> bocLottieCommonDialog.update(bocLottieDialogEnum, hint, 0, () -> {
+    private void next(AppCompatActivity appCompatActivity, BocLottieDialogEnum bocLottieDialogEnum, String hint) {
+        MmkvKit.defaultMmkv().encode(RestoreConstant.RESTORE_$_ACCOUNT_DATA_BASE_TABLE, true);
+        TimerKit.getInstance().execute(appCompatActivity, RestoreKit.getInstance().delay, () -> RestoreKit.getInstance().bocLottieCommonDialog.update(bocLottieDialogEnum, hint, 0, () -> {
             // 预存或恢复功能数据库表
-            RestoreFunctionDataBaseTableKit.getInstance().preStoreOrRestoreFunctionDataBaseTable(appCompatActivity, bocLottieCommonDialog);
+            RestoreFunctionDataBaseTableKit.getInstance().preStoreOrRestoreFunctionDataBaseTable(appCompatActivity);
         }));
     }
 
