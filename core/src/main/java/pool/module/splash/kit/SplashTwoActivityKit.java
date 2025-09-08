a0181332d7e1c914135c5383fd928249546d3dc1
snpmyn <snpmyn@126.com>
2025-09-08 17:58:07 +0800

    提交

--- core/src/main/java/pool/module/splash/kit/SplashTwoActivityKit.java
+++ core/src/main/java/pool/module/splash/kit/SplashTwoActivityKit.java
@@ -3,6 +3,7 @@ package pool.module.splash.kit;
 import android.animation.ValueAnimator;
 import android.text.TextUtils;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -27,15 +28,34 @@ public class SplashTwoActivityKit {
      * @param lottieAnimationView 控件
      * @param splashActivityKit   闪屏页配套元件
      */
-    public void execute(AppCompatActivity appCompatActivity, LottieAnimationView lottieAnimationView, SplashActivityKit splashActivityKit) {
-        String assetName = MmkvKit.defaultMmkv().decodeString(PoolConstant.SPLASH_$_ANIMATION);
-        if (MmkvKit.defaultMmkv().decodeBool(PoolConstant.SPLASH_$_USE_DEFAULT_ANIMATION) || TextUtils.isEmpty(assetName)) {
-            assetName = "lottie_animation_splash_default";
-        }
-        LottieKit.getInstance().useWithAsset(lottieAnimationView, assetName + ".json", ValueAnimator.INFINITE, null);
+    public void execute(@NonNull AppCompatActivity appCompatActivity, LottieAnimationView lottieAnimationView, SplashActivityKit splashActivityKit) {
+        LottieKit.getInstance().useWithAsset(lottieAnimationView, getAnimationAssetName() + ".json", ValueAnimator.INFINITE, null);
         HandlerKit.getInstance().postDelayed(() -> {
             LottieKit.getInstance().endAnimation(lottieAnimationView);
             splashActivityKit.execute(appCompatActivity);
+            saveAnimationAssetName();
         }, Long.parseLong(appCompatActivity.getString(R.string.PoolSplashDuration)));
+    }
+
+    /**
+     * 获取动画资源名
+     *
+     * @return 动画资源名
+     */
+    private String getAnimationAssetName() {
+        String animationAssetName = MmkvKit.defaultMmkv().decodeString(PoolConstant.SPLASH_$_ANIMATION);
+        if (TextUtils.isEmpty(animationAssetName)) {
+            animationAssetName = "lottie_animation_splash_default";
+        }
+        return animationAssetName;
+    }
+
+    /**
+     * 保存动画资源名
+     */
+    private void saveAnimationAssetName() {
+        if (TextUtils.isEmpty(MmkvKit.defaultMmkv().decodeString(PoolConstant.SPLASH_$_ANIMATION))) {
+            MmkvKit.defaultMmkv().encode(PoolConstant.SPLASH_$_ANIMATION, "lottie_animation_splash_default");
+        }
     }
 }
