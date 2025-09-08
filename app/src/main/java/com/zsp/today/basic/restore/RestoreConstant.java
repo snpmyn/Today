92148890ba9811fef464e17cf2d885a41ef8a4ee
snpmyn <snpmyn@126.com>
2025-09-08 17:45:45 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/basic/restore/RestoreConstant.java
@@ -0,0 +1,22 @@
+package com.zsp.today.basic.restore;
+
+/**
+ * Created on 2025/9/8.
+ *
+ * @author 郑少鹏
+ * @desc 恢复常量
+ */
+public class RestoreConstant {
+    /**
+     * 恢复（账目数据库表）
+     */
+    public static final String RESTORE_$_ACCOUNT_DATA_BASE_TABLE = "RestoreAccountDataBaseTable";
+    /**
+     * 恢复（功能数据库表）
+     */
+    public static final String RESTORE_$_FUNCTION_DATA_BASE_TABLE = "RestoreFunctionDataBaseTable";
+    /**
+     * 恢复（险情数据库表）
+     */
+    public static final String RESTORE_$_DANGEROUS_DATA_BASE_TABLE = "RestoreDangerousDataBaseTable";
+}
