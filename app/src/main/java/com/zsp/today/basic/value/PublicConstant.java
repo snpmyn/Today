84b455777e7dcde358bb8f8706301c20ccf9f5c9
snpmyn <snpmyn@126.com>
2025-09-09 21:18:48 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/basic/value/PublicConstant.java
@@ -0,0 +1,14 @@
+package com.zsp.today.basic.value;
+
+/**
+ * Created on 2025/9/9.
+ *
+ * @author 郑少鹏
+ * @desc 公共常量
+ */
+public class PublicConstant {
+    /**
+     * 延时时长
+     */
+    public static long DELAY_DURATION = 500L;
+}
