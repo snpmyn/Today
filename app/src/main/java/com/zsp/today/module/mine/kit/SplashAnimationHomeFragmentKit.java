fdcb8d0762047d279b039bb7512d837ded2039c3
snpmyn <snpmyn@126.com>
2025-09-20 11:08:44 +0800

    提交

--- app/src/main/java/com/zsp/today/module/mine/kit/SplashAnimationHomeFragmentKit.java
+++ app/src/main/java/com/zsp/today/module/mine/kit/SplashAnimationHomeFragmentKit.java
@@ -46,13 +46,13 @@ public class SplashAnimationHomeFragmentKit {
         StatusManagerKit.statusJudge(statusManager, true, null);
         // 数据
         List<SplashAnimationListBean> splashAnimationListBeanList = new ArrayList<>(6);
-        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie_animation_splash_one"));
-        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie_animation_splash_two"));
-        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie_animation_splash_three"));
-        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie_animation_splash_four"));
-        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie_animation_splash_five"));
-        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie_animation_splash_six"));
-        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie_animation_splash_seven"));
+        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie/lottie_animation_splash_one"));
+        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie/lottie_animation_splash_two"));
+        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie/lottie_animation_splash_three"));
+        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie/lottie_animation_splash_four"));
+        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie/lottie_animation_splash_five"));
+        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie/lottie_animation_splash_six"));
+        splashAnimationListBeanList.add(new SplashAnimationListBean("lottie/lottie_animation_splash_seven"));
         // 控件
         RecyclerViewConfigure recyclerViewConfigure = new RecyclerViewConfigure(appCompatActivity, recyclerView);
         recyclerViewConfigure.gridLayout(2, 48, true, true, false);
