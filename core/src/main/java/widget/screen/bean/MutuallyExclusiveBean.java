4ed0b3807fddd48e7bc355ccbc9d359079e71f22
snpmyn <snpmyn@126.com>
2025-09-27 21:18:10 +0800

    提交

---
+++ core/src/main/java/widget/screen/bean/MutuallyExclusiveBean.java
@@ -0,0 +1,37 @@
+package widget.screen.bean;
+
+/**
+ * Created on 2019/9/26.
+ *
+ * @author 郑少鹏
+ * @desc 互斥
+ */
+public class MutuallyExclusiveBean {
+    /**
+     * 组 ID
+     */
+    private final String groupId;
+    /**
+     * 类别
+     */
+    private final String classification;
+
+    /**
+     * constructor
+     *
+     * @param groupId        组 ID
+     * @param classification 类别
+     */
+    public MutuallyExclusiveBean(String groupId, String classification) {
+        this.groupId = groupId;
+        this.classification = classification;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+}
---
+++ core/src/main/java/widget/screen/bean/UnfoldAndFoldBean.java
@@ -0,0 +1,49 @@
+package widget.screen.bean;
+
+import java.util.List;
+
+/**
+ * Created on 2019/9/28.
+ *
+ * @author 郑少鹏
+ * @desc 展开 / 折叠数据
+ */
+public class UnfoldAndFoldBean {
+    /**
+     * 主控类别
+     */
+    private final String activeControlClassification;
+    /**
+     * 主控条件
+     */
+    private final String activeControlCondition;
+    /**
+     * 被控类别数据
+     */
+    private final List<String> passiveControlClassificationList;
+
+    /**
+     * constructor
+     *
+     * @param activeControlClassification      主控类别
+     * @param activeControlCondition           主控条件
+     * @param passiveControlClassificationList 被控类别数据
+     */
+    public UnfoldAndFoldBean(String activeControlClassification, String activeControlCondition, List<String> passiveControlClassificationList) {
+        this.activeControlClassification = activeControlClassification;
+        this.activeControlCondition = activeControlCondition;
+        this.passiveControlClassificationList = passiveControlClassificationList;
+    }
+
+    public String getActiveControlClassification() {
+        return activeControlClassification;
+    }
+
+    public String getActiveControlCondition() {
+        return activeControlCondition;
+    }
+
+    public List<String> getPassiveControlClassificationList() {
+        return passiveControlClassificationList;
+    }
+}
