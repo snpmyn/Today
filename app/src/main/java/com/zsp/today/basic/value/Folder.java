f2d53188ba6a376a5c2b38921847c89e18859d0d
snpmyn <snpmyn@126.com>
2025-09-27 20:04:28 +0800

    提交

--- app/src/main/java/com/zsp/today/basic/value/Folder.java
+++ app/src/main/java/com/zsp/today/basic/value/Folder.java
@@ -20,32 +20,46 @@ public class Folder {
     /**
      * 崩溃
      */
-    public static String CRASH = getExternalFilesDir("today/crash").getAbsolutePath();
+    public static String CRASH = getExternalPrivateDirectory("today/crash").getAbsolutePath();
     /**
      * 账单
      */
     public static String ACCOUNT = getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath();
     /**
      * 内备份
+     * <p>
+     * 为何 cache 下出现 backups
+     * 路径包含 backup 时系统可能默认触发临时缓存机制 → cache/backups
+     * 内部会尝试写到 cache 目录作临时备份后再复制到持久化目录
+     * 此即 cache/backups 被创建原因
+     * 目的避免写入文件失败导致数据丢失或作为缓存加速
+     * 两者同时存在正常
+     * <p>
+     * 该机制主要在 release 版生效
+     * 因系统认为 release 版更正式
+     * 要保证数据不丢失
+     * <p>
+     * 而 debug 版通常 bypass 此类保护机制
+     * 故不会在 cache 下创建 backups
      */
-    public static String INTERNAL_BACKUP = getExternalFilesDir("today/backup").getAbsolutePath();
+    public static String INTERNAL_BACKUP = getExternalPrivateDirectory("today/backup").getAbsolutePath();
     /**
      * 外备份
      */
     public static String EXTERNAL_BACKUP = getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath();
 
     /**
-     * 获取外部文件目录
+     * 获取外部私有目录
      *
      * @param type 类型
      * @return 文件
      */
-    private static File getExternalFilesDir(String type) {
+    private static File getExternalPrivateDirectory(String type) {
         return App.getAppInstance().getExternalFilesDir(type);
     }
 
     /**
-     * 获取外部存储公共目录
+     * 获取外部公共目录
      *
      * @param type 类型
      * @return 文件
