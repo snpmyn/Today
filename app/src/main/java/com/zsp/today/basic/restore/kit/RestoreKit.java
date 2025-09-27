e9d87c3112c3244037f143aeb45e4011c2ba888b
snpmyn <snpmyn@126.com>
2025-09-27 20:05:16 +0800

    提交

--- app/src/main/java/com/zsp/today/basic/restore/kit/RestoreKit.java
+++ app/src/main/java/com/zsp/today/basic/restore/kit/RestoreKit.java
@@ -2,6 +2,13 @@ package com.zsp.today.basic.restore.kit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.zsp.today.BuildConfig;
+import com.zsp.today.basic.value.Folder;
+import com.zsp.today.module.account.database.AccountDataBaseTable;
+import com.zsp.today.module.dangerous.database.DangerousDataBaseTable;
+
+import java.io.File;
+
 import widget.dialog.bocdialog.lottie.BocLottieCommonDialog;
 
 /**
@@ -15,6 +22,14 @@ public class RestoreKit {
      * BOC Lottie 普通对话框
      */
     public BocLottieCommonDialog bocLottieCommonDialog;
+    /**
+     * 账目数据库表文件绝对路径
+     */
+    public String accountDataBaseTableFileAbsolutePath = Folder.EXTERNAL_BACKUP + File.separator + AccountDataBaseTable.class.getSimpleName() + BuildConfig.BACKUP_SUFFIX + ".json";
+    /**
+     * 险情数据库表文件绝对路径
+     */
+    public String dangerousDataBaseTableFileAbsolutePath = Folder.EXTERNAL_BACKUP + File.separator + DangerousDataBaseTable.class.getSimpleName() + BuildConfig.BACKUP_SUFFIX + ".json";
 
     public static RestoreKit getInstance() {
         return InstanceHolder.INSTANCE;
@@ -23,10 +38,21 @@ public class RestoreKit {
     /**
      * 恢复
      *
-     * @param appCompatActivity 活动
+     * @param appCompatActivity  活动
+     * @param restoreKitListener 恢复配套元件监听
      */
-    public void restore(AppCompatActivity appCompatActivity) {
-        RestoreAccountDataBaseTableKit.getInstance().restoreAccountDataBaseTable(appCompatActivity);
+    public void restore(AppCompatActivity appCompatActivity, RestoreKitListener restoreKitListener) {
+        RestoreAccountDataBaseTableKit.getInstance().restoreAccountDataBaseTable(appCompatActivity, restoreKitListener);
+    }
+
+    /**
+     * 恢复配套元件监听
+     */
+    public interface RestoreKitListener {
+        /**
+         * 结束
+         */
+        void end();
     }
 
     private static final class InstanceHolder {
