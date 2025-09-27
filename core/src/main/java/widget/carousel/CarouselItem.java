730e24ac4bae524c020f299c5165cc0ffcbaae04
snpmyn <snpmyn@126.com>
2025-09-27 21:02:14 +0800

    提交

--- core/src/main/java/widget/carousel/CarouselItem.java
+++ core/src/main/java/widget/carousel/CarouselItem.java
@@ -11,17 +11,27 @@ public class CarouselItem {
      * 位图资源 ID
      */
     public int drawableResId;
+    /**
+     * 位图描述
+     */
+    public String drawableDescribe;
 
     /**
      * constructor
      *
-     * @param drawableResId 位图资源 ID
+     * @param drawableResId    位图资源 ID
+     * @param drawableDescribe 位图描述
      */
-    public CarouselItem(int drawableResId) {
+    public CarouselItem(int drawableResId, String drawableDescribe) {
         this.drawableResId = drawableResId;
+        this.drawableDescribe = drawableDescribe;
     }
 
     public int getDrawableResId() {
         return this.drawableResId;
+    }
+
+    public String getDrawableDescribe() {
+        return drawableDescribe;
     }
 }
