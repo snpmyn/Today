bfe8fb9a239915cf22a6470519c28f97c969acd3
snpmyn <snpmyn@126.com>
2025-09-29 16:26:57 +0800

    提交

---
+++ core/src/main/java/widget/ion/config/IonInitConfig.java
@@ -0,0 +1,40 @@
+package widget.ion.config;
+
+/**
+ * Created on 2025/9/23.
+ *
+ * @author 郑少鹏
+ * @desc Ion 初始化配置
+ */
+public class IonInitConfig {
+    /**
+     * GET
+     */
+    public static final String GET = "GET";
+    /**
+     * PUT
+     */
+    public static final String PUT = "PUT";
+    /**
+     * POST
+     * <p>
+     * 常用请求方式
+     */
+    public static final String POST = "POST";
+    /**
+     * DELETE
+     */
+    public static final String DELETE = "DELETE";
+    /**
+     * 超时
+     */
+    public static final int TIME_OUT = 5000;
+    /**
+     * 日志级别
+     */
+    public static final int LOG_LEVEL = 1;
+    /**
+     * 日志标志
+     */
+    public static final String LOG_TAG = "IonInitConfig";
+}
