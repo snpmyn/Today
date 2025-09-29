5cae61c7ca1c947d619aea586a73f08dc8ef9c92
snpmyn <snpmyn@126.com>
2025-09-29 17:40:16 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/basic/version/bean/VersionInfoBean.java
@@ -0,0 +1,204 @@
+package com.zsp.today.basic.version.bean;
+
+import widget.ion.base.BaseIonBean;
+
+/**
+ * Created on 2025/9/23.
+ *
+ * @author 郑少鹏
+ * @desc 版本信息
+ */
+public class VersionInfoBean extends BaseIonBean {
+    private int code;
+    private String message;
+    private Data data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    public static class Data {
+        private String buildBuildVersion;
+        private String forceUpdateVersion;
+        private String forceUpdateVersionNo;
+        private boolean needForceUpdate;
+        private String downloadURL;
+        private boolean buildHaveNewVersion;
+        private String buildVersionNo;
+        private String buildVersion;
+        private String buildDescription;
+        private String buildUpdateDescription;
+        private String buildShortcutUrl;
+        private String appURl;
+        private String appKey;
+        private String buildKey;
+        private String buildName;
+        private String buildIcon;
+        private String buildFileKey;
+        private String buildFileSize;
+
+        public String getBuildBuildVersion() {
+            return buildBuildVersion;
+        }
+
+        public void setBuildBuildVersion(String buildBuildVersion) {
+            this.buildBuildVersion = buildBuildVersion;
+        }
+
+        public String getForceUpdateVersion() {
+            return forceUpdateVersion;
+        }
+
+        public void setForceUpdateVersion(String forceUpdateVersion) {
+            this.forceUpdateVersion = forceUpdateVersion;
+        }
+
+        public String getForceUpdateVersionNo() {
+            return forceUpdateVersionNo;
+        }
+
+        public void setForceUpdateVersionNo(String forceUpdateVersionNo) {
+            this.forceUpdateVersionNo = forceUpdateVersionNo;
+        }
+
+        public boolean isNeedForceUpdate() {
+            return needForceUpdate;
+        }
+
+        public void setNeedForceUpdate(boolean needForceUpdate) {
+            this.needForceUpdate = needForceUpdate;
+        }
+
+        public String getDownloadURL() {
+            return downloadURL;
+        }
+
+        public void setDownloadURL(String downloadURL) {
+            this.downloadURL = downloadURL;
+        }
+
+        public boolean isBuildHaveNewVersion() {
+            return buildHaveNewVersion;
+        }
+
+        public void setBuildHaveNewVersion(boolean buildHaveNewVersion) {
+            this.buildHaveNewVersion = buildHaveNewVersion;
+        }
+
+        public String getBuildVersionNo() {
+            return buildVersionNo;
+        }
+
+        public void setBuildVersionNo(String buildVersionNo) {
+            this.buildVersionNo = buildVersionNo;
+        }
+
+        public String getBuildVersion() {
+            return buildVersion;
+        }
+
+        public void setBuildVersion(String buildVersion) {
+            this.buildVersion = buildVersion;
+        }
+
+        public String getBuildDescription() {
+            return buildDescription;
+        }
+
+        public void setBuildDescription(String buildDescription) {
+            this.buildDescription = buildDescription;
+        }
+
+        public String getBuildUpdateDescription() {
+            return buildUpdateDescription;
+        }
+
+        public void setBuildUpdateDescription(String buildUpdateDescription) {
+            this.buildUpdateDescription = buildUpdateDescription;
+        }
+
+        public String getBuildShortcutUrl() {
+            return buildShortcutUrl;
+        }
+
+        public void setBuildShortcutUrl(String buildShortcutUrl) {
+            this.buildShortcutUrl = buildShortcutUrl;
+        }
+
+        public String getAppURl() {
+            return appURl;
+        }
+
+        public void setAppURl(String appURl) {
+            this.appURl = appURl;
+        }
+
+        public String getAppKey() {
+            return appKey;
+        }
+
+        public void setAppKey(String appKey) {
+            this.appKey = appKey;
+        }
+
+        public String getBuildKey() {
+            return buildKey;
+        }
+
+        public void setBuildKey(String buildKey) {
+            this.buildKey = buildKey;
+        }
+
+        public String getBuildName() {
+            return buildName;
+        }
+
+        public void setBuildName(String buildName) {
+            this.buildName = buildName;
+        }
+
+        public String getBuildIcon() {
+            return buildIcon;
+        }
+
+        public void setBuildIcon(String buildIcon) {
+            this.buildIcon = buildIcon;
+        }
+
+        public String getBuildFileKey() {
+            return buildFileKey;
+        }
+
+        public void setBuildFileKey(String buildFileKey) {
+            this.buildFileKey = buildFileKey;
+        }
+
+        public String getBuildFileSize() {
+            return buildFileSize;
+        }
+
+        public void setBuildFileSize(String buildFileSize) {
+            this.buildFileSize = buildFileSize;
+        }
+    }
+}
