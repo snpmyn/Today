77464daa9be2ce1b24ebcf68ca24310d21b054a6
snpmyn <snpmyn@126.com>
2025-10-02 22:14:06 +0800

    提交

--- app/src/main/java/com/zsp/today/module/account/bean/AccountMonthListBean.java
+++ app/src/main/java/com/zsp/today/module/account/bean/AccountMonthListBean.java
@@ -19,18 +19,30 @@ public class AccountMonthListBean {
      * 总金额
      */
     private final String totalAmount;
+    /**
+     * 数量
+     */
+    private final int count;
+    /**
+     * 最大类目和金额
+     */
+    private final String maxCategoryAndAmount;
 
     /**
      * constructor
      *
-     * @param year        年
-     * @param month       月
-     * @param totalAmount 总金额
+     * @param year                 年
+     * @param month                月
+     * @param totalAmount          总金额
+     * @param count                数量
+     * @param maxCategoryAndAmount 最大类目和金额
      */
-    public AccountMonthListBean(String year, String month, String totalAmount) {
+    public AccountMonthListBean(String year, String month, String totalAmount, int count, String maxCategoryAndAmount) {
         this.year = year;
         this.month = month;
         this.totalAmount = totalAmount;
+        this.count = count;
+        this.maxCategoryAndAmount = maxCategoryAndAmount;
     }
 
     public String getYear() {
@@ -43,5 +55,13 @@ public class AccountMonthListBean {
 
     public String getTotalAmount() {
         return totalAmount;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getMaxCategoryAndAmount() {
+        return maxCategoryAndAmount;
     }
 }
