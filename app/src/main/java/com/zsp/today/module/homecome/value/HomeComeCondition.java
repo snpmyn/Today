670ac9878a7feb70a33773cfc665cc824b4fd1b7
snpmyn <snpmyn@126.com>
2025-10-13 16:44:05 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/module/homecome/value/HomeComeCondition.java
@@ -0,0 +1,11 @@
+package com.zsp.today.module.homecome.value;
+
+/**
+ * Created on 2025/10/11.
+ *
+ * @author 郑少鹏
+ * @desc 归心条件
+ */
+public class HomeComeCondition {
+    public static String HOME_COME_PHONE_NUMBER_AND_CALL_AND_CALENDAR_TYPE_AND_DEATH_YEAR_AND_DEATH_MONTH_AND_DEATH_DAY = "phoneNumber = ? and call = ? and calendarType = ? and deathYear = ? and deathMonth = ? and deathDay = ?";
+}
