927bffa96c50bc0f5cacbbe47f5d5e01eb52b7a7
snpmyn <snpmyn@126.com>
2025-10-14 22:04:37 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/module/homecome/value/HomeComeConstant.java
@@ -0,0 +1,14 @@
+package com.zsp.today.module.homecome.value;
+
+/**
+ * Created on 2025/10/14.
+ *
+ * @author 郑少鹏
+ * @desc 归心常量
+ */
+public class HomeComeConstant {
+    /**
+     * 归心页（背景图片资源）
+     */
+    public static final String HOME_COME_ACTIVITY_$_BACKGROUND_IMAGE_RESOURCE = "HomeComeActivityBackgroundImageResource";
+}
