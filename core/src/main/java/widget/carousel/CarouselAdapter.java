72877f49355fbf37c70d3f9d89cb71334842524b
snpmyn <snpmyn@126.com>
2025-10-14 21:54:05 +0800

    提交

--- core/src/main/java/widget/carousel/CarouselAdapter.java
+++ core/src/main/java/widget/carousel/CarouselAdapter.java
@@ -3,12 +3,15 @@ package widget.carousel;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 
 import com.zsp.core.R;
+
+import util.density.DensityUtils;
 
 /**
  * @decs: 轮播适配器
@@ -17,6 +20,22 @@ import com.zsp.core.R;
  * @version: v 1.0
  */
 public class CarouselAdapter extends ListAdapter<CarouselItem, CarouselViewHolder> {
+    /**
+     * 左边距
+     */
+    private final int left;
+    /**
+     * 上边距
+     */
+    private final int top;
+    /**
+     * 右边距
+     */
+    private final int right;
+    /**
+     * 下边距
+     */
+    private final int bottom;
     /**
      * 轮播条目布局资源 ID
      */
@@ -44,22 +63,20 @@ public class CarouselAdapter extends ListAdapter<CarouselItem, CarouselViewHolde
     /**
      * constructor
      *
+     * @param left             左边距
+     * @param top              上边距
+     * @param right            右边距
+     * @param bottom           下边距
      * @param carouselListener 轮播监听
      */
-    public CarouselAdapter(CarouselListener carouselListener) {
-        this(carouselListener, R.layout.carousel_item);
-    }
-
-    /**
-     * constructor
-     *
-     * @param carouselListener        轮播监听
-     * @param carouselItemLayoutResId 轮播条目布局资源 ID
-     */
-    public CarouselAdapter(CarouselListener carouselListener, int carouselItemLayoutResId) {
+    public CarouselAdapter(int left, int top, int right, int bottom, CarouselListener carouselListener) {
         super(carouselItemCallback);
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
         this.carouselListener = carouselListener;
-        this.carouselItemLayoutResId = carouselItemLayoutResId;
+        this.carouselItemLayoutResId = R.layout.carousel_item;
     }
 
     @NonNull
@@ -71,6 +88,30 @@ public class CarouselAdapter extends ListAdapter<CarouselItem, CarouselViewHolde
     @Override
     public void onBindViewHolder(@NonNull CarouselViewHolder carouselViewHolder, int pos) {
         carouselViewHolder.bind(getItem(pos));
+        // 适配横 / 竖向
+        int itemCount = getItemCount();
+        int marginLeft = 0, marginTop = 0, marginRight = 0, marginBottom = 0;
+        if (pos == 0) {
+            // 头条
+            // 右 / 下边距
+            marginRight = right;
+            marginBottom = bottom;
+        } else if (pos == (itemCount - 1)) {
+            // 末条
+            // 左 / 上边距
+            marginLeft = left;
+            marginTop = top;
+        } else {
+            // 中条
+            // 四边距
+            marginLeft = left;
+            marginTop = top;
+            marginRight = right;
+            marginBottom = bottom;
+        }
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        layoutParams.setMargins(DensityUtils.dipToPxByInt(marginLeft), DensityUtils.dipToPxByInt(marginTop), DensityUtils.dipToPxByInt(marginRight), DensityUtils.dipToPxByInt(marginBottom));
+        carouselViewHolder.itemView.setLayoutParams(layoutParams);
         carouselViewHolder.itemView.setOnHoverListener((v, event) -> {
             switch (event.getAction()) {
                 case MotionEvent.ACTION_HOVER_ENTER:
