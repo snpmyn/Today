e97d4a3030ab77a1784c2d5c08eed46e323a692f
snpmyn <snpmyn@126.com>
2025-10-17 18:53:29 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/module/lingfang/value/LingFangConstant.java
@@ -0,0 +1,14 @@
+package com.zsp.today.module.lingfang.value;
+
+/**
+ * Created on 2025/10/17.
+ *
+ * @author 郑少鹏
+ * @desc 灵方常量
+ */
+public class LingFangConstant {
+    /**
+     * 灵方页（背景图片资源）
+     */
+    public static final String LING_FANG_ACTIVITY_$_BACKGROUND_IMAGE_RESOURCE = "LingFangActivityBackgroundImageResource";
+}
