a5c24771834d42e142a37c477211b9bbbf8838ac
snpmyn <snpmyn@126.com>
2026-01-16 11:38:22 +0800

    提交

--- app/src/main/java/com/zsp/today/module/mine/MineFragment.java
+++ app/src/main/java/com/zsp/today/module/mine/MineFragment.java
@@ -2,21 +2,40 @@ package com.zsp.today.module.mine;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.zsp.today.R;
-import com.zsp.today.module.mine.fragment.MineChildFragment;
+import com.zsp.today.module.mine.kit.MineFragmentKit;
 
 import org.jetbrains.annotations.NotNull;
 
 import pool.base.BasePoolFragment;
+import widget.appbarlayout.listener.BaseAppBarLayoutStateChangeListener;
+import widget.toast.ToastKit;
 
 /**
- * Created on 2020/12/14
+ * Created on 2020/12/17
  *
  * @author zsp
  * @desc 我的碎片
  */
-public class MineFragment extends BasePoolFragment {
+public class MineFragment extends BasePoolFragment implements View.OnClickListener {
+    private LottieAnimationView mineFragmentLavHeadPortrait;
+    private TextView mineFragmentTvNickName;
+    private MaterialToolbar mineFragmentMt;
+    private AppBarLayout mineFragmentAbl;
+    private RecyclerView mineFragmentRv;
+    /**
+     * 我的碎片配套元件
+     */
+    private MineFragmentKit mineFragmentKit;
+
     public static @NotNull MineFragment newInstance() {
         Bundle bundle = new Bundle();
         MineFragment fragment = new MineFragment();
@@ -25,94 +44,76 @@ public class MineFragment extends BasePoolFragment {
     }
 
     /**
-     * 布局资源 ID
+     * 获取布局 ID
      *
-     * @return 布局资源 ID
+     * @return 布局 ID
      */
     @Override
-    protected int layoutResId() {
+    protected int getLayoutId() {
         return R.layout.fragment_mine;
     }
 
     /**
-     * 第一 Fragment 否
-     *
-     * @return 第一 Fragment 否
-     */
-    @Override
-    protected boolean areFirstFragment() {
-        return false;
-    }
-
-    /**
-     * EventBus 注册
-     * <p>
-     * onDestroyView 反注册。
-     */
-    @Override
-    protected void eventBusRegister() {
-
-    }
-
-    /**
-     * 初始控件
-     * <p>
-     * 此处仅设 Toolbar 标题、返回箭头等轻量 UI 操作。
+     * 初始视图
      *
      * @param view 视图
      */
     @Override
-    protected void stepUi(View view) {
-
+    protected void initView(@NonNull View view) {
+        mineFragmentLavHeadPortrait = view.findViewById(R.id.mineFragmentLavHeadPortrait);
+        mineFragmentTvNickName = view.findViewById(R.id.mineFragmentTvNickName);
+        mineFragmentMt = view.findViewById(R.id.mineFragmentMt);
+        mineFragmentAbl = view.findViewById(R.id.mineFragmentAbl);
+        mineFragmentRv = view.findViewById(R.id.mineFragmentRv);
     }
 
     /**
-     * Fragment 对用户可见时调
-     */
-    @Override
-    protected void visibleToUser() {
-
-    }
-
-    /**
-     * 开始加载
+     * 第一次可见
      * <p>
-     * 某些场景需懒加载，如 FragmentAdapter 懒加载、同级 Fragment 切换懒加载。
-     * 库自 0.8 提供 onLazyInitView(Bundle saveInstanceState) 使用。
+     * 只调一次
+     * <p>
+     * 网络请求
+     * 耗时任务
+     * 初始化数据
      */
     @Override
-    protected void startLoadOnLazyInitView() {
-        if (null == findChildFragment(MineChildFragment.class)) {
-            loadRootFragment(R.id.mineFragmentFl, MineChildFragment.newInstance());
+    protected void onFragmentFirstVisible() {
+        initConfiguration();
+        setListener();
+        startLogic();
+    }
+
+    private void initConfiguration() {
+        mineFragmentKit = new MineFragmentKit();
+    }
+
+    private void setListener() {
+        mineFragmentAbl.addOnOffsetChangedListener(new BaseAppBarLayoutStateChangeListener() {
+            @Override
+            public void onStateChanged(AppBarLayout appBarLayout, State state) {
+                if (state == State.COLLAPSED) {
+                    // 折叠
+                    mineFragmentKit.setNickName(baseLazyFragmentAppCompatActivity, mineFragmentTvNickName, mineFragmentMt, false);
+                } else {
+                    // 中间
+                    mineFragmentMt.setTitle("");
+                }
+            }
+        });
+        mineFragmentLavHeadPortrait.setOnClickListener(this);
+    }
+
+    private void startLogic() {
+        // 设置昵称
+        mineFragmentKit.setNickName(baseLazyFragmentAppCompatActivity, mineFragmentTvNickName, mineFragmentMt, true);
+        // 展示
+        mineFragmentKit.display(baseLazyFragmentAppCompatActivity, mineFragmentRv);
+    }
+
+    @Override
+    public void onClick(@NonNull View v) {
+        if (v.getId() == R.id.mineFragmentLavHeadPortrait) {
+            ToastKit.showShort(getString(R.string.app_name));
         }
-    }
-
-    /**
-     * 开始加载
-     * <p>
-     * 此处设 Listener、各 Adapter、请求数据等。
-     * onDestroyView 释放。
-     */
-    @Override
-    protected void startLoadOnEnterAnimationEnd() {
-
-    }
-
-    /**
-     * Fragment 对用户不可见时调
-     */
-    @Override
-    protected void invisibleToUser() {
-
-    }
-
-    /**
-     * EventBus 反注册
-     * <p>
-     * onCreateView 注册。
-     */
-    @Override
-    protected void eventBusUnregister() {
-
     }
 }
