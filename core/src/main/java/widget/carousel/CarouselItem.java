4bf6150cfcefef5fb09bae4fc6185ae93e442c73
snpmyn <snpmyn@126.com>
2026-01-27 14:06:51 +0800

    提交

--- core/src/main/java/widget/carousel/CarouselItem.java
+++ core/src/main/java/widget/carousel/CarouselItem.java
@@ -1,5 +1,9 @@
 package widget.carousel;
 
+import androidx.annotation.Nullable;
+
+import java.util.Objects;
+
 /**
  * @decs: 轮播条目
  * @author: 郑少鹏
@@ -8,40 +12,67 @@ package widget.carousel;
  */
 public class CarouselItem {
     /**
-     * 位图资源 ID
+     * 显示图像
      */
-    public int drawableResId;
+    public boolean showImage;
     /**
-     * 位图资源名
+     * 轮播资源 ID
      */
-    public String drawableResName;
+    public int carouselResId;
     /**
-     * 位图描述
+     * 轮播标题
      */
-    public String drawableDescribe;
+    public String carouselTitle;
+    /**
+     * 轮播描述
+     */
+    public String carouselDescribe;
 
     /**
      * constructor
      *
-     * @param drawableResId    位图资源 ID
-     * @param drawableResName  位图资源名
-     * @param drawableDescribe 位图描述
+     * @param showImage        显示图像
+     * @param carouselResId    轮播资源 ID
+     * @param carouselTitle    轮播标题
+     * @param carouselDescribe 轮播描述
      */
-    public CarouselItem(int drawableResId, String drawableResName, String drawableDescribe) {
-        this.drawableResId = drawableResId;
-        this.drawableResName = drawableResName;
-        this.drawableDescribe = drawableDescribe;
+    public CarouselItem(boolean showImage, int carouselResId, String carouselTitle, String carouselDescribe) {
+        this.showImage = showImage;
+        this.carouselResId = carouselResId;
+        this.carouselTitle = carouselTitle;
+        this.carouselDescribe = carouselDescribe;
     }
 
-    public int getDrawableResId() {
-        return drawableResId;
+    public boolean isShowImage() {
+        return showImage;
     }
 
-    public String getDrawableResName() {
-        return drawableResName;
+    public int getCarouselResId() {
+        return carouselResId;
     }
 
-    public String getDrawableDescribe() {
-        return drawableDescribe;
+    public String getCarouselTitle() {
+        return carouselTitle;
+    }
+
+    public String getCarouselDescribe() {
+        return carouselDescribe;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof CarouselItem)) {
+            return false;
+        }
+        CarouselItem carouselItem = (CarouselItem) obj;
+        return ((showImage == carouselItem.showImage) && (carouselResId == carouselItem.carouselResId) && Objects.equals(carouselTitle, carouselItem.carouselTitle) && Objects.equals(carouselDescribe, carouselItem.carouselDescribe));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(showImage, carouselResId, carouselTitle, carouselDescribe);
     }
 }
