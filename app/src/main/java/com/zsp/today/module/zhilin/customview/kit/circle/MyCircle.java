32b531ed5f9c1e6e82e02dccdd2687315d668e08
snpmyn <snpmyn@126.com>
2026-02-07 23:09:17 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/module/zhilin/customview/kit/circle/MyCircle.java
@@ -0,0 +1,48 @@
+package com.zsp.today.module.zhilin.customview.kit.circle;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+/**
+ * Created on 2020/7/2.
+ *
+ * @author zsp
+ * @desc 圆
+ */
+public class MyCircle extends View {
+    private Paint paint;
+
+    public MyCircle(Context context) {
+        super(context);
+    }
+
+    public MyCircle(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        paint = new Paint();
+    }
+
+    public MyCircle(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
+        paint.setAntiAlias(true);
+        paint.setColor(ContextCompat.getColor(getContext(), com.zsp.core.R.color.color_FFEE6002));
+        paint.setAlpha(20);
+        canvas.drawCircle(Integer.valueOf(getWidth() / 2).floatValue(), Integer.valueOf(getHeight() / 2).floatValue(), 240.0F, paint);
+    }
+}
