138d33d2f7a643dd6c641feaa8ee40047c94c230
snpmyn <snpmyn@126.com>
2026-02-08 10:08:56 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/module/zhilin/customview/kit/oval/MyOval.java
@@ -0,0 +1,48 @@
+package com.zsp.today.module.zhilin.customview.kit.oval;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+/**
+ * Created on 2020/7/3.
+ *
+ * @author zsp
+ * @desc 椭圆
+ */
+public class MyOval extends View {
+    private Paint paint;
+
+    public MyOval(Context context) {
+        super(context);
+    }
+
+    public MyOval(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        paint = new Paint();
+    }
+
+    public MyOval(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
+        paint.setAntiAlias(true);
+        paint.setColor(ContextCompat.getColor(getContext(), com.zsp.core.R.color.color_FFEE6002));
+        canvas.drawOval(Integer.valueOf(getWidth() / 2 - 240).floatValue(), Integer.valueOf(getHeight() / 2 - 120).floatValue(),
+                Integer.valueOf(getWidth() / 2 + 240).floatValue(), Integer.valueOf(getHeight() / 2 + 120).floatValue(), paint);
+    }
+}
