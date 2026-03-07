5f84ba08d0f89b671f92a04ca3522c58afb356e9
snpmyn <snpmyn@126.com>
2026-03-07 18:51:37 +0800

    提交

---
+++ core/src/main/java/widget/carousel/CarouselType.java
@@ -0,0 +1,22 @@
+package widget.carousel;
+
+/**
+ * Created on 2026/3/7.
+ *
+ * @author 郑少鹏
+ * @desc 轮播类型
+ */
+public enum CarouselType {
+    /**
+     * 图片
+     */
+    IMAGE,
+    /**
+     * 文本
+     */
+    TEXT,
+    /**
+     * HTML
+     */
+    HTML
+}
