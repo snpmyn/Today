bb17b596ca5fe6dc3316aa2fc4956468768ed1ab
snpmyn <snpmyn@126.com>
2026-03-13 15:07:27 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/module/poetry/TokenModel.java
@@ -0,0 +1,32 @@
+package com.zsp.today.module.poetry;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created on 2026/3/10.
+ *
+ * @author 郑少鹏
+ * @desc token 模型
+ */
+public class TokenModel {
+    @SerializedName("status")
+    private String status;
+    @SerializedName("data")
+    private String data;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
