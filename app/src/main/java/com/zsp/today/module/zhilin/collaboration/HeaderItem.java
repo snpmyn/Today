24864b72c0cd2968a1a0b15cda52664431a65601
snpmyn <snpmyn@126.com>
2026-04-26 12:27:11 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/module/zhilin/collaboration/HeaderItem.java
@@ -0,0 +1,64 @@
+package com.zsp.today.module.zhilin.collaboration;
+
+/**
+ * Created on 2026/4/23.
+ *
+ * @author 郑少鹏
+ * @desc 头条目
+ */
+public class HeaderItem extends ListItem {
+    /**
+     * 识别状态
+     */
+    public int recognizeState;
+    /**
+     * 需要展开
+     */
+    public boolean needExpand;
+    /**
+     * 数量
+     */
+    public int count;
+
+    /**
+     * constructor
+     *
+     * @param recognizeState 识别状态
+     */
+    public HeaderItem(int recognizeState) {
+        this.recognizeState = recognizeState;
+    }
+
+    /**
+     * 获取标题
+     *
+     * @return 标题
+     */
+    public String getTitle() {
+        String title;
+        switch (recognizeState) {
+            case 0:
+                title = "青海";
+                break;
+            case 1:
+                title = "新疆";
+                break;
+            case 2:
+                title = "西藏";
+                break;
+            default:
+                title = "";
+        }
+        return title + " (" + count + ")";
+    }
+
+    /**
+     * 获取类型
+     *
+     * @return 类型
+     */
+    @Override
+    public int getType() {
+        return TYPE_HEADER;
+    }
+}
