9b82625147248cf7fdccb18990fe1ba4f9270e2b
snpmyn <snpmyn@126.com>
2026-04-26 12:26:42 +0800

    提交

---
+++ app/src/main/java/com/zsp/today/module/zhilin/collaboration/ListItem.java
@@ -0,0 +1,25 @@
+package com.zsp.today.module.zhilin.collaboration;
+
+/**
+ * Created on 2026/4/23.
+ *
+ * @author 郑少鹏
+ * @desc 列表条目
+ */
+public abstract class ListItem {
+    /**
+     * 头类型
+     */
+    public static final int TYPE_HEADER = 0;
+    /**
+     * 子类型
+     */
+    public static final int TYPE_CHILD = 1;
+
+    /**
+     * 获取类型
+     *
+     * @return 类型
+     */
+    public abstract int getType();
+}
