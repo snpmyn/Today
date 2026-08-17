ef6a508f42a7f738fb00c9296e67862e5da6dae2
snpmyn <snpmyn@126.com>
2026-08-17 10:13:49 +0800

    升级 MMKV 配置

--- core/src/main/java/util/mmkv/MmkvInitConfigure.java
+++ core/src/main/java/util/mmkv/MmkvInitConfigure.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.getkeepsafe.relinker.ReLinker;
 import com.tencent.mmkv.MMKV;
-import com.tencent.mmkv.MMKVContentChangeNotification;
 import com.tencent.mmkv.MMKVHandler;
 import com.tencent.mmkv.MMKVLogLevel;
 
@@ -20,26 +19,23 @@ import timber.log.Timber;
  */
 public class MmkvInitConfigure {
     /**
-     * 初始 MMKV
+     * 初始化 MMKV
      *
-     * @param application                   应用
-     * @param debug                         调试模式
-     * @param mmkvHandler                   MMKV 处理
-     * @param mmkvContentChangeNotification MMKV 内容改变通知
+     * @param application 应用实例
+     * @param debug       是否开启调试模式
+     * @param mmkvHandler MMKV 处理器 [接管日志与异常]
      */
-    public static void initMmkv(@NotNull Application application, boolean debug, MMKVHandler mmkvHandler, MMKVContentChangeNotification mmkvContentChangeNotification) {
+    public static void initMmkv(@NotNull Application application, boolean debug, MMKVHandler mmkvHandler) {
         String dir = application.getFilesDir().getAbsolutePath() + "/mmkv";
-        String rootDir = MMKV.initialize(application, dir, libName -> ReLinker.loadLibrary(application, libName), MMKVLogLevel.LevelInfo);
+        MMKVLogLevel logLevel = debug ? MMKVLogLevel.LevelInfo : MMKVLogLevel.LevelNone;
+        String rootDir = MMKV.initialize(application, dir, libName -> ReLinker.loadLibrary(application, libName), logLevel, mmkvHandler);
         Timber.d("mmkv root - %s", rootDir);
-        MMKV.setLogLevel(debug ? MMKVLogLevel.LevelInfo : MMKVLogLevel.LevelNone);
-        MMKV.registerHandler(mmkvHandler);
-        MMKV.registerContentChangeNotify(mmkvContentChangeNotification);
     }
 
     /**
      * 退出
      * <p>
-     * 官方示例于 Activity 之 onDestroy() 调。
+     * 官方示例于 Activity 之 onDestroy() 调
      */
     public static void exit() {
         MMKV.onExit();
