404d3197bcf8385d611c43cef67ad2ca8763dadc
snpmyn <snpmyn@126.com>
2026-09-17 11:44:00 +0800

    CameraX 对接 UVC 摄像头

--- app/src/main/java/com/zsp/today/module/camera/kit/CameraActivityKit.java
+++ app/src/main/java/com/zsp/today/module/camera/kit/CameraActivityKit.java
@@ -80,6 +80,55 @@ public class CameraActivityKit {
     }
 
     /**
+     * 初始化分辨率下拉选择框
+     *
+     * @param context               上下文
+     * @param lifecycleOwner        生命周期拥有者
+     * @param activityCameraBinding ActivityCameraBinding
+     */
+    public void setupResolutionSpinner(Context context, LifecycleOwner lifecycleOwner, @NonNull ActivityCameraBinding activityCameraBinding) {
+        if (selectedCameraId == null) {
+            return;
+        }
+        // 1. 动态查询选定摄像头支持的真实分辨率
+        supportedResolutionList = cameraController.getSupportedResolutions(context, selectedCameraId);
+        if (supportedResolutionList.isEmpty()) {
+            Timber.tag(TAG).w("未查询到摄像头 CameraID: %s 支持的分辨率列表", selectedCameraId);
+            return;
+        }
+        // 2. 格式化为可视化分辨率文本
+        List<String> resolutionsStr = new ArrayList<>();
+        for (Size size : supportedResolutionList) {
+            resolutionsStr.add(size.getWidth() + " x " + size.getHeight());
+        }
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, resolutionsStr);
+        stringArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // 3. 避免重新设置 Adapter 时触发旧监听器的误选择逻辑
+        activityCameraBinding.cameraActivitySpinnerSwitchResolution.setOnItemSelectedListener(null);
+        activityCameraBinding.cameraActivitySpinnerSwitchResolution.setAdapter(stringArrayAdapter);
+        // 4. 默认选中最高分辨率
+        activityCameraBinding.cameraActivitySpinnerSwitchResolution.setSelection(0);
+        selectedResolution = supportedResolutionList.get(0);
+        // 5. 监听下拉选择切换
+        activityCameraBinding.cameraActivitySpinnerSwitchResolution.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Size newSize = supportedResolutionList.get(position);
+                // 用户选中分辨率发生变化时重启相机更新流参数
+                if (!newSize.equals(selectedResolution)) {
+                    selectedResolution = newSize;
+                    startCamera(context, lifecycleOwner, activityCameraBinding);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    /**
      * 启动相机
      *
      * @param context               上下文
@@ -126,57 +175,6 @@ public class CameraActivityKit {
             setupResolutionSpinner(context, lifecycleOwner, activityCameraBinding);
             startCamera(context, lifecycleOwner, activityCameraBinding);
         }).setCancelable(false).show();
-    }
-
-    /**
-     * 初始化分辨率下拉选择框
-     *
-     * @param context               上下文
-     * @param lifecycleOwner        生命周期拥有者
-     * @param activityCameraBinding ActivityCameraBinding
-     */
-    public void setupResolutionSpinner(Context context, LifecycleOwner lifecycleOwner, @NonNull ActivityCameraBinding activityCameraBinding) {
-        if (selectedCameraId == null) {
-            return;
-        }
-        // 1. 动态查询选定摄像头支持的真实分辨率
-        supportedResolutionList = cameraController.getSupportedResolutions(context, selectedCameraId);
-        if (supportedResolutionList.isEmpty()) {
-            Timber.tag(TAG).w("未查询到摄像头 CameraID: %s 支持的分辨率列表", selectedCameraId);
-            return;
-        }
-        // 2. 格式化为可视化分辨率文本
-        // 例如 3840 x 2160
-        List<String> resolutionsStr = new ArrayList<>();
-        for (Size size : supportedResolutionList) {
-            resolutionsStr.add(size.getWidth() + " x " + size.getHeight());
-        }
-        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, resolutionsStr);
-        stringArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // 3. 避免重新设置 Adapter 时触发旧监听器的误选择逻辑
-        activityCameraBinding.cameraActivitySpinnerSwitchResolution.setOnItemSelectedListener(null);
-        activityCameraBinding.cameraActivitySpinnerSwitchResolution.setAdapter(stringArrayAdapter);
-        // 4. 默认选中最高分辨率
-        // 索引 0
-        activityCameraBinding.cameraActivitySpinnerSwitchResolution.setSelection(0);
-        selectedResolution = supportedResolutionList.get(0);
-        // 5. 监听下拉选择切换
-        activityCameraBinding.cameraActivitySpinnerSwitchResolution.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Size newSize = supportedResolutionList.get(position);
-                // 用户选中分辨率发生变化时重启相机更新流参数
-                if (!newSize.equals(selectedResolution)) {
-                    selectedResolution = newSize;
-                    startCamera(context, lifecycleOwner, activityCameraBinding);
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
     }
 
     /**
