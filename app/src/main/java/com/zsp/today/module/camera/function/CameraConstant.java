a56857043e795609a68dd00b2e1a01d28f0a7216
snpmyn <snpmyn@126.com>
2026-09-21 16:12:40 +0800

    CameraX 对接 UVC 摄像头

---
+++ app/src/main/java/com/zsp/today/module/camera/function/CameraConstant.java
@@ -0,0 +1,14 @@
+package com.zsp.today.module.camera.function;
+
+/**
+ * Created on 2026/9/21.
+ *
+ * @author 郑少鹏
+ * @desc 相机常量
+ */
+public class CameraConstant {
+    /**
+     * 相机目标旋转角度
+     */
+    public static final String CAMERA_$_TARGET_ROTATION = "CameraTargetRotation";
+}
