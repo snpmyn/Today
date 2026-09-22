98d78ba09350dbe09f695abec5fdedd42f3ff532
snpmyn <snpmyn@126.com>
2026-09-22 15:54:11 +0800

    CameraX 对接 UVC 摄像头

--- app/src/main/java/com/zsp/today/module/camera/kit/CameraActivityKit.java
+++ app/src/main/java/com/zsp/today/module/camera/kit/CameraActivityKit.java
@@ -56,10 +56,6 @@ public class CameraActivityKit {
      */
     private List<Size> supportedResolutionList = new ArrayList<>();
     /**
-     * 分辨率是否正在初始化
-     */
-    private boolean isResolutionInitializing = false;
-    /**
      * 已选分辨率
      */
     private Size selectedResolution = null;
@@ -112,6 +108,33 @@ public class CameraActivityKit {
     }
 
     /**
+     * 显示相机选择对话框
+     *
+     * @param context               上下文
+     * @param lifecycleOwner        生命周期拥有者
+     * @param activityCameraBinding ActivityCameraBinding
+     */
+    public void showCameraSelectDialog(Context context, LifecycleOwner lifecycleOwner, ActivityCameraBinding activityCameraBinding) {
+        if (ListUtils.listIsEmpty(cameraIdList)) {
+            // 获取系统底层注册的所有相机 ID 列表
+            cameraIdList = CameraManagerKit.getAvailableCameraIds(context);
+        }
+        if (ListUtils.listIsEmpty(cameraIdList)) {
+            ToastKt.showToast("未检测到摄像头");
+            return;
+        }
+        String[] items = new String[cameraIdList.size()];
+        for (int i = 0; i < cameraIdList.size(); i++) {
+            items[i] = ("Camera ID: " + cameraIdList.get(i));
+        }
+        new MaterialAlertDialogBuilder(context).setTitle("选择要打开的摄像头").setItems(items, (dialog, which) -> {
+            selectedCameraId = cameraIdList.get(which);
+            // 执行
+            execute(context, lifecycleOwner, activityCameraBinding);
+        }).setCancelable(false).show();
+    }
+
+    /**
      * 执行
      *
      * @param context               上下文
@@ -138,14 +161,10 @@ public class CameraActivityKit {
         if (selectedCameraId == null) {
             return;
         }
-        // 标记开始初始化
-        // 拦截 Spinner 设置 Adapter 阶段的自动伪触发
-        isResolutionInitializing = true;
         // 1. 获取指定相机 ID 支持的原生分辨率列表
         supportedResolutionList = CameraManagerKit.getSupportedResolutions(context, selectedCameraId);
         if (ListUtils.listIsEmpty(supportedResolutionList)) {
             Timber.tag(LogKit.TAG).w("未查询到摄像头 CameraID: %s 支持的分辨率列表", selectedCameraId);
-            isResolutionInitializing = false;
             return;
         }
         // 2. 格式化为可视化分辨率文本
@@ -153,39 +172,23 @@ public class CameraActivityKit {
         for (Size size : supportedResolutionList) {
             resolutionsStr.add(size.getWidth() + " x " + size.getHeight());
         }
-        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, resolutionsStr);
-        stringArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, resolutionsStr);
         // 3. 避免重新设置 Adapter 时触发旧监听器的误选择逻辑
-        activityCameraBinding.cameraActivitySpinnerSwitchResolution.setOnItemSelectedListener(null);
-        activityCameraBinding.cameraActivitySpinnerSwitchResolution.setAdapter(stringArrayAdapter);
+        activityCameraBinding.cameraActivityMactvSwitchResolution.setOnItemClickListener(null);
+        activityCameraBinding.cameraActivityMactvSwitchResolution.setAdapter(stringArrayAdapter);
         // 4. 默认选中最高分辨率
-        activityCameraBinding.cameraActivitySpinnerSwitchResolution.setSelection(0, true);
+        activityCameraBinding.cameraActivityMactvSwitchResolution.setText(resolutionsStr.get(0), false);
         selectedResolution = supportedResolutionList.get(0);
-        // 5. 延迟恢复监听防抖
-        // 通过 View.post 将任务推入主线程 MessageQueue 末尾
-        // 确保在 Spinner 内部 RequestLayout 与 View 树测量绘制引发的原生伪回调执行完毕后再重置标志位为 false
-        activityCameraBinding.cameraActivitySpinnerSwitchResolution.post(() -> isResolutionInitializing = false);
-        // 6. 监听下拉选择切换
-        activityCameraBinding.cameraActivitySpinnerSwitchResolution.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (isResolutionInitializing) {
-                    return;
-                }
-                if ((position < 0) || (position >= supportedResolutionList.size())) {
-                    return;
-                }
-                Size newResolution = supportedResolutionList.get(position);
-                if (!newResolution.equals(selectedResolution)) {
-                    selectedResolution = newResolution;
-                    // 启动相机
-                    startCamera(context, lifecycleOwner, activityCameraBinding);
-                }
+        // 5. 监听下拉选择切换
+        activityCameraBinding.cameraActivityMactvSwitchResolution.setOnItemClickListener((parent, view, position, id) -> {
+            if ((position < 0) || (position >= supportedResolutionList.size())) {
+                return;
             }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
+            Size newResolution = supportedResolutionList.get(position);
+            if (!newResolution.equals(selectedResolution)) {
+                selectedResolution = newResolution;
+                // 设置分辨率
+                cameraController.setResolution(context, lifecycleOwner, activityCameraBinding.cameraActivityMcv, activityCameraBinding.cameraActivityPv, selectedResolution, selectedRotation);
             }
         });
     }
@@ -201,7 +204,7 @@ public class CameraActivityKit {
             return;
         }
         // 标记开始初始化
-        // 拦截 Spinner 设置 Adapter 阶段的自动伪触发
+        // 拦截 Spinner 设置 Adapter 阶段自动触发
         isRotationInitializing = true;
         // 1. 设置支持的旋转角度列表
         // 关联 Surface 角度常量值
@@ -246,7 +249,7 @@ public class CameraActivityKit {
                 Integer newRotation = supportedRotationList.get(position);
                 if (!newRotation.equals(selectedRotation)) {
                     selectedRotation = newRotation;
-                    // 存储目标旋转角度
+                    // 设置目标旋转角度
                     cameraController.setTargetRotation(activityCameraBinding.cameraActivityPv, selectedRotation);
                 }
             }
@@ -256,33 +259,6 @@ public class CameraActivityKit {
 
             }
         });
-    }
-
-    /**
-     * 显示相机选择对话框
-     *
-     * @param context               上下文
-     * @param lifecycleOwner        生命周期拥有者
-     * @param activityCameraBinding ActivityCameraBinding
-     */
-    public void showCameraSelectDialog(Context context, LifecycleOwner lifecycleOwner, ActivityCameraBinding activityCameraBinding) {
-        if (ListUtils.listIsEmpty(cameraIdList)) {
-            // 获取系统底层注册的所有相机 ID 列表
-            cameraIdList = CameraManagerKit.getAvailableCameraIds(context);
-        }
-        if (ListUtils.listIsEmpty(cameraIdList)) {
-            ToastKt.showToast("未检测到摄像头");
-            return;
-        }
-        String[] items = new String[cameraIdList.size()];
-        for (int i = 0; i < cameraIdList.size(); i++) {
-            items[i] = "Camera ID: " + cameraIdList.get(i);
-        }
-        new MaterialAlertDialogBuilder(context).setTitle("选择要打开的摄像头").setItems(items, (dialog, which) -> {
-            selectedCameraId = cameraIdList.get(which);
-            // 执行
-            execute(context, lifecycleOwner, activityCameraBinding);
-        }).setCancelable(false).show();
     }
 
     /**
@@ -296,19 +272,17 @@ public class CameraActivityKit {
         CameraInitCallback cameraInitCallback = new CameraInitCallback() {
             @Override
             public void onCameraInitSuccess() {
-                Timber.tag(LogKit.TAG).i("相机绑定成功 Camera ID: %s", selectedCameraId);
+                Timber.tag(LogKit.TAG).i("相机启动成功 Camera ID: %s", selectedCameraId);
             }
 
             @Override
             public void onCameraInitError(@NonNull Throwable throwable) {
+                Timber.tag(LogKit.TAG).e(throwable, "相机启动失败: %s", throwable.getMessage());
                 activityCameraBinding.getRoot().post(() -> activityCameraBinding.cameraActivityTv.setText(String.format(Locale.getDefault(), context.getString(R.string.formatFpsNoValue), "--")));
-                ToastKt.showToast("相机启动失败: " + throwable.getMessage());
             }
         };
         // 启动相机
-        cameraController.startCamera(context, lifecycleOwner, activityCameraBinding.cameraActivityMcv, activityCameraBinding.cameraActivityPv, selectedCameraId, selectedResolution, cameraInitCallback);
-        // 已选旋转角度
-        this.selectedRotation = CameraConfigKit.resolveTargetRotation(selectedCameraId, activityCameraBinding.cameraActivityPv, CameraDeviceKit.checkIsUvcCamera(context, null, selectedCameraId));
+        cameraController.startCamera(context, lifecycleOwner, activityCameraBinding.cameraActivityMcv, activityCameraBinding.cameraActivityPv, selectedCameraId, selectedResolution, selectedRotation, cameraInitCallback);
     }
 
     /**
@@ -322,19 +296,19 @@ public class CameraActivityKit {
         cameraController.capture(context, activityCameraBinding.cameraActivityPv, new CameraCaptureCallback() {
             @Override
             public void onCameraCaptureSuccess(File photoFile) {
-                Timber.tag(LogKit.TAG).i("保存路径: %s", photoFile.getAbsolutePath());
+                Timber.tag(LogKit.TAG).i("拍照成功: %s", photoFile.getAbsolutePath());
                 activityCameraBinding.getRoot().post(() -> {
                     activityCameraBinding.cameraActivityMtCapture.setEnabled(true);
-                    ToastKt.showToast("抓拍成功: " + photoFile.getName());
+                    ToastKt.showToast("拍照成功: " + photoFile.getName());
                 });
             }
 
             @Override
             public void onCameraCaptureError(ImageCaptureException imageCaptureException) {
-                Timber.tag(LogKit.TAG).e(imageCaptureException, "抓拍失败: %s", imageCaptureException.getMessage());
+                Timber.tag(LogKit.TAG).e(imageCaptureException, "拍照失败: %s", imageCaptureException.getMessage());
                 activityCameraBinding.getRoot().post(() -> {
                     activityCameraBinding.cameraActivityMtCapture.setEnabled(true);
-                    ToastKt.showToast("抓拍失败: " + imageCaptureException.getMessage());
+                    ToastKt.showToast("拍照失败: " + imageCaptureException.getMessage());
                 });
             }
         });
@@ -346,6 +320,6 @@ public class CameraActivityKit {
      * @param activityCameraBinding ActivityCameraBinding
      */
     public void release(ActivityCameraBinding activityCameraBinding) {
-        cameraController.release(activityCameraBinding != null ? activityCameraBinding.cameraActivityPv : null);
+        cameraController.release((activityCameraBinding != null) ? activityCameraBinding.cameraActivityPv : null);
     }
 }
